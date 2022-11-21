d014eb31132c5589a14cb4a930181a5ec8c21730
ngoc.tran3 <ngoc.tran3@sotatek.com>
2022-11-21 11:10:17 +0700

    fix prettier config and font

--- src/commons/menus.ts
+++ src/commons/menus.ts
@@ -1,65 +1,33 @@
-
-export const menus = [
-    { title: "DID", href: "https://fb.me" },
-    { title: "Supply Chain", href: "https://fb.me" },
-    {
-        title: "Blockchain",
-        mega: true,
-        children: [
-            {
-                title: "Tools",
-                children: [
-                    { title: "Token Registry", href: "/Token-Registry" },
-                    { title: "Daedalous Wallet", href: "/Daedalous-Wallet" },
-                    { title: "Cardano RTView", href: "/Cardano-RTView" },
-                    { title: "...", href: "/..." },
-                    { title: "...", href: "/..." }
-                ]
-            },
-            {
-                title: "Documents",
-                children: [
-                    { title: "Stakepool", href: "/Stakepool" },
-                    { title: "About us", href: "/About-us" },
-                    { title: "...", href: "/..." },
-                    { title: "...", href: "/..." }
-                ]
-            },
-            {
-                title: "Developer",
-                children: [
-                    { title: "Ouroboros Protocol", href: "/Ouroboros-Protocol" },
-                    { title: "Cardano Nodes", href: "/Cardano-Nodes" },
-                    { title: "Github", href: "https://github.com" },
-                    { title: "Developer Tools", href: "/Developer-Tools" }
-                ]
-            }
-        ]
-    },
-    {
-        title: "Resource",
-        children: [
-            { title: "Charts and Stats", href: "/Charts-and-Stats" },
-            { title: "Top Statsitics", href: "/Top-Statsitics" },
-            { title: "Developer API", href: "/Developer-API" },
-            { title: "Builder Tools", href: "/Builder-Tools" },
-            { title: "Project Showcase", href: "/Project-Showcase" }
-        ]
-    },
-    {
-        title: "Delegate Pools",
-        children: [
-            { title: "Delegation Pools", href: "/Delegation-Pools" },
-            { title: "Stake key registration", href: "/Stake-key-registration" },
-            { title: "Pool Registration", href: "/Pool-Registration" }
-        ]
-    },
-    {
-        title: "More",
-        children: [
-            { title: "Cardano Mainet", href: "/Cardano-Mainet" },
-            { title: "Cardano Testnet", href: "/Cardano-Testnet" },
-            { title: "Cardano Preview Testnet", href: "/Cardano-Preview-Testnet" }
-        ]
-    },
+interface Menu {
+  title: string;
+  href?: string;
+  mega?: boolean;
+  children?: Menu[];
+}
+export const menus: Menu[] = [
+  { title: "DID", href: "/did" },
+  { title: "Supply Chain", href: "/chains" },
+  {
+    title: "Blockchain",
+    children: [
+      { title: "Epoch", href: "/epochs" },
+      { title: "Blocks", href: "/blocks" },
+      { title: "Transactions API", href: "/transactions" },
+      { title: "Tokens", href: "/tokens" },
+      { title: "Top Delegators", href: "/delegators" },
+      { title: "Top Addresses", href: "/sddresses" },
+    ],
+  },
+  {
+    title: "Delegate Pools",
+    children: [
+      { title: "Delegation Pools", href: "/delegation-pools" },
+      { title: "Stake key registration", href: "/Stake-key-registration" },
+      { title: "Pool Registration", href: "/Pool-Registration" },
+    ],
+  },
+  {
+    title: "Tool",
+    href: "/tools",
+  },
 ];
