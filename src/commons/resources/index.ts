a566e8ddb5f9730014260758c0e675e1b049064f
ngoc.tran3 <ngoc.tran3@sotatek.com>
2022-12-14 19:01:12 +0700

    feat: side bar extenal menu

--- src/commons/resources/index.ts
+++ src/commons/resources/index.ts
@@ -36,6 +36,8 @@ import SupplyChainMenuIcon from "./icons/menus/supplychain.svg";
 import BlockChainMenuIcon from "./icons/menus/blockchain.svg";
 import DelegatePoolMenuIcon from "./icons/menus/delegate.svg";
 import ToolMenuIcon from "./icons/menus/tool.svg";
+import BrowserIcon from "./icons/menus/browser.svg";
+import ResourcesIcon from "./icons/menus/resources.svg";
 import RocketIcon from "./icons/rocket.svg";
 import CubeIcon from "./icons/cube.svg";
 import CheckCircleIcon from "./icons/check-circle.svg";
@@ -120,4 +122,6 @@ export {
   InfoIcon,
   HighestIcon,
   LowestIcon,
+  BrowserIcon,
+  ResourcesIcon,
 };
