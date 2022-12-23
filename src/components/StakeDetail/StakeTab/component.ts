8c5f0488fc62574d90faa241acfec4133683cab6
taitm <truongtai10102k@gmail.com>
2022-12-23 15:24:08 +0700

    update api

--- src/components/StakeDetail/StakeTab/component.ts
+++ src/components/StakeDetail/StakeTab/component.ts
@@ -6,3 +6,13 @@ export const TitleTab = styled(Box)(({ theme }) => ({
   fontFamily: '"Space Mono", monospace, sans-serif',
   fontSize: "1.125rem",
 }));
+export const LabelStatus = styled(Box)(({ theme }) => ({
+  textTransform: "uppercase",
+  padding: `${theme.spacing(1)} ${theme.spacing(3)}`,
+  fontFamily: '"Space Mono", monospace, sans-serif',
+  fontWeight: "bold",
+  fontSize: "0.875rem",
+  borderRadius: 4,
+  height: "60%",
+  width: "max-content",
+}));
