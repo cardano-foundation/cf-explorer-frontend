c8b60eb5dceffae8c6c45af4c4cad5ff3b566de5
taitm <truongtai10102k@gmail.com>
2023-02-10 15:56:40 +0700

    update bookmark

--- src/types/bookmark.d.ts
+++ src/types/bookmark.d.ts
@@ -1,7 +1,10 @@
+import { NETWORK_TYPES } from "../commons/utils/constants";
+
 interface BookMark {
   createdDate?: string;
   id?: number;
   keyword: string;
   type: "BLOCK" | "EPOCH" | "TRANSACTION" | "ADDRESS" | "POOL" | "STAKE_KEY";
   urlPage?: string;
+  network: NETWORK_TYPES[keyof NETWORK_TYPES];
 }
