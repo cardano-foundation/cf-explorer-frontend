c921fce82217612a1c736f6c5ead0ba049b8e704
ngoc.tran3 <ngoc.tran3@sotatek.com>
2023-02-15 14:26:27 +0700

    fix: MET-224: Real time price rate

--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -6,36 +6,16 @@ import { useHistory } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
 import themes from "./themes";
 import { setOnDetailView } from "./stores/user";
-import { setUsdMarket, setCurrentEpoch } from "./stores/system";
-import useFetch from "./commons/hooks/useFetch";
-import { API, EXTEN_API } from "./commons/utils/api";
-import { useLocalStorage } from "react-use";
+import { SystemLoader } from "./components/SystemLoader";
 interface Props {
   children: React.ReactNode;
 }
 
 const AppContainer: React.FC<Props> = props => {
-  const { userData, theme } = useSelector(({ user }: RootState) => user);
-  const isLogin = !!userData?.username;
-  const [, setBookmark] = useLocalStorage<string[]>("bookmark", []);
+  const { theme } = useSelector(({ user }: RootState) => user);
   const history = useHistory();
   const lastPath = useRef(history.location.pathname);
   const { children } = props;
-  const currentEpoch = useFetch<EpochCurrentType>(API.EPOCH.CURRENT_EPOCH);
-  const usdMarket = useFetch<CardanoMarket[]>(`${EXTEN_API.COINGECKO.PRICE}?vs_currency=usd&ids=cardano`);
-  const { data: dataBookmark } = useFetch<string[]>(isLogin ? `/bookmark/find-all-key` : "", undefined, true);
-
-  useEffect(() => {
-    if (usdMarket.data?.[0]) setUsdMarket(usdMarket.data[0]);
-  }, [usdMarket.data]);
-
-  useEffect(() => {
-    if (dataBookmark) setBookmark(dataBookmark);
-  }, [dataBookmark]);
-
-  useEffect(() => {
-    if (currentEpoch.data) setCurrentEpoch(currentEpoch.data);
-  }, [currentEpoch.data]);
 
   useEffect(() => {
     const unlisten = history.listen(() => {
@@ -52,6 +32,7 @@ const AppContainer: React.FC<Props> = props => {
 
   return (
     <ThemeProvider theme={themes[theme]}>
+      <SystemLoader />
       <div data-theme={theme}>
         <CustomLayout>{children}</CustomLayout>
       </div>
