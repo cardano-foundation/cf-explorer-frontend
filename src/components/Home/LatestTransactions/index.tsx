082a0d0f7a10a1cbb3ff9b57788d757c547e4902
dai nguyen <dai@mmj.vn>
2023-04-13 17:04:00 +0700

    feature/MET-461: update transactions table

--- src/components/Home/LatestTransactions/index.tsx
+++ src/components/Home/LatestTransactions/index.tsx
@@ -1,7 +1,6 @@
 import { Box, Grid, Skeleton } from "@mui/material";
 import React, { useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
-import useFetchList from "../../../commons/hooks/useFetchList";
 import { BlankBlueIcon, ADAIcon } from "../../../commons/resources";
 import { details, routers } from "../../../commons/routers";
 import { API } from "../../../commons/utils/api";
@@ -63,13 +62,13 @@ const LatestTransactions: React.FC = () => {
                 );
               })
             : data?.map(item => {
-                const { hash, fromAddress, toAddress, blockNo, amount, status } = item;
+                const { hash, fromAddress, toAddress, blockNo, amount, status, time, epochNo, epochSlotNo } = item;
                 return (
                   <Grid item xl lg={3} xs={6} key={hash}>
                     <Item onClick={e => handleClicktWithoutAnchor(e, () => history.push(details.transaction(hash)))}>
                       <ItemHeader>
                         <PriceImage src={ADAIcon} alt="check green" />
-                        <Box display={"flex"} flexDirection={"column"} rowGap={'4px'} alignItems={"end"}>
+                        <Box display={"flex"} flexDirection={"column"} rowGap={"4px"} alignItems={"end"}>
                           <HeaderStatus status={status as TRANSACTION_STATUS}>{status}</HeaderStatus>
                           <PriveValue>{formatADAFull(amount)}</PriveValue>
                         </Box>
@@ -88,6 +87,16 @@ const LatestTransactions: React.FC = () => {
                           <Link to={details.block(blockNo)}>
                             <BlockNo>{blockNo}</BlockNo>
                           </Link>
+                        </RowItem>
+                        <RowItem>
+                          <small>Epoch: </small>
+                          <Link to={details.epoch(epochNo)}>
+                            <BlockNo>{epochNo}</BlockNo>
+                          </Link>
+                        </RowItem>
+                        <RowItem>
+                          <small>Slot: </small>
+                          <small>{epochSlotNo}</small>
                         </RowItem>
                         {fromAddress?.slice(0, 1).map(add => {
                           return (
@@ -117,6 +126,11 @@ const LatestTransactions: React.FC = () => {
                             </RowItem>
                           );
                         })}
+
+                        <RowItem>
+                          <small>Timestamp: </small>
+                          <small>{time}</small>
+                        </RowItem>
                       </ItemDetail>
                     </Item>
                   </Grid>
