147b6b0b4accf5d94c9ed62bd55bcbd4595dd339
Sotatek-HuyDoan <132533358+Sotatek-HuyDoan@users.noreply.github.com>
2023-05-08 10:29:33 -0700

    feature: MET-615 responsive Tabular table SPO2

--- src/pages/SPOLifecycle/styles.ts
+++ src/pages/SPOLifecycle/styles.ts
@@ -14,6 +14,10 @@ export const StakeId = styled("span")(({ theme }) => ({
 }));
 
 export const ButtonGroup = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  height: "44px",
+  marginTop: "23px !important",
   background: "#E7E8EA",
   padding: "3px 2px",
   margin: `0 ${theme.spacing(2)}`,
@@ -21,6 +25,10 @@ export const ButtonGroup = styled(Box)(({ theme }) => ({
   borderBottomLeftRadius: "20px",
   borderTopRightRadius: "20px",
   borderBottomRightRadius: "20px",
+  [theme.breakpoints.down("md")]: {
+    width: "90px",
+    marginTop: "10px !important",
+  },
 }));
 export const ButtonSwitch = styled(IconButton)<{ active: number }>(({ theme, active }) => ({
   margin: "0 2px",
@@ -40,5 +48,49 @@ export const ButtonReport = styled(Button)(({ theme }) => ({
   borderRadius: "8px",
   ":hover": {
     background: alpha(theme.palette.grey[700], 0.8),
+  },
+  [theme.breakpoints.down("md")]: {
+    width: "328px",
+  },
+}));
+
+export const BoxContainerStyled = styled(Box)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItem: "center",
+  flexDirection: "row",
+  [theme.breakpoints.down("md")]: {
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+  },
+}));
+
+export const BoxSwitchContainer = styled(Box)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "space-between",
+  // [theme.breakpoints.down("md")]: {
+  //   justifyContent: "space-between",
+  //   alignItems: "space-between",
+  // },
+}));
+
+export const BoxItemStyled = styled(Box)(({ theme }) => ({
+  display: "flex",
+  [theme.breakpoints.down("md")]: {
+    flexDirection: "column",
+  },
+}));
+
+export const BoxSwitch = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+}));
+
+export const ButtonReportContainer = styled(Button)(({ theme }) => ({
+  display: "flex",
+  [theme.breakpoints.down("md")]: {
+    justifyContent: "start",
   },
 }));
