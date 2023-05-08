4177ca41b44a7db452e14f9baf296fa85eab41c8
Sotatek-HuyDoan <132533358+Sotatek-HuyDoan@users.noreply.github.com>
2023-05-08 11:00:45 -0700

    Revert "feature: MET-615 responsive Tabular table SPO2"
    This reverts commit 147b6b0b4accf5d94c9ed62bd55bcbd4595dd339.

--- src/pages/SPOLifecycle/index.tsx
+++ src/pages/SPOLifecycle/index.tsx
@@ -6,7 +6,18 @@ import { getShortHash } from "../../commons/utils/helper";
 import CopyButton from "../../components/commons/CopyButton";
 import SPOLifecycleComponent from "../../components/StakingLifeCycle/SPOLifecycle";
 
-import { ButtonGroup, ButtonReport, ButtonSwitch, StakeId, StyledContainer } from "./styles";
+import {
+  BoxContainerStyled,
+  BoxItemStyled,
+  BoxSwitch,
+  BoxSwitchContainer,
+  ButtonGroup,
+  ButtonReport,
+  ButtonReportContainer,
+  ButtonSwitch,
+  StakeId,
+  StyledContainer,
+} from "./styles";
 
 import { ReactComponent as ChartMode } from "../../commons/resources/icons/Staking/ChartMode.svg";
 import { ReactComponent as TableMode } from "../../commons/resources/icons/Staking/TableMode.svg";
@@ -67,7 +78,7 @@ const SPOLifecycle = () => {
   }, []);
   return (
     <StyledContainer ref={containerRef}>
-      <Box display={"flex"} justifyContent={"space-between"} alignItems={"center"}>
+      <BoxContainerStyled>
         <Box>
           <Box component={"h2"} mb={0}>
             Staking Lifecycle For
@@ -80,21 +91,25 @@ const SPOLifecycle = () => {
             <CopyButton text={poolId} />
           </Box>
         </Box>
-        <Box display={"flex"} alignItems={"center"}>
-          <Box color={({ palette }) => palette.grey[400]}>
-            Switch to {mode === "timeline" ? "tablular" : "timeline"} view
-          </Box>
-          <ButtonGroup>
-            <ButtonSwitch active={+(mode === "timeline")} onClick={() => setMode("timeline")}>
-              <ChartMode fill={mode === "timeline" ? "#fff" : "#344054"} />
-            </ButtonSwitch>
-            <ButtonSwitch active={+(mode === "tablular")} onClick={() => setMode("tablular")}>
-              <TableMode fill={mode === "tablular" ? "#fff" : "#344054"} />
-            </ButtonSwitch>
-          </ButtonGroup>
-          {mode === "tablular" && <ButtonReport onClick={() => setOpen(true)}>Compose report</ButtonReport>}
-        </Box>
-      </Box>
+        <BoxItemStyled>
+          <BoxSwitchContainer>
+            <BoxSwitch color={({ palette }) => palette.grey[400]}>
+              Switch to {mode === "timeline" ? "tablular" : "timeline"} view
+            </BoxSwitch>
+            <ButtonGroup>
+              <ButtonSwitch active={+(mode === "timeline")} onClick={() => setMode("timeline")}>
+                <ChartMode fill={mode === "timeline" ? "#fff" : "#344054"} />
+              </ButtonSwitch>
+              <ButtonSwitch active={+(mode === "tablular")} onClick={() => setMode("tablular")}>
+                <TableMode fill={mode === "tablular" ? "#fff" : "#344054"} />
+              </ButtonSwitch>
+            </ButtonGroup>
+          </BoxSwitchContainer>
+          <ButtonReportContainer>
+            {mode === "tablular" && <ButtonReport onClick={() => setOpen(true)}>Compose report</ButtonReport>}
+          </ButtonReportContainer>
+        </BoxItemStyled>
+      </BoxContainerStyled>
 
       <Box>
         {mode === "timeline" && (
