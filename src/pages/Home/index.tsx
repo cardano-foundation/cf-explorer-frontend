b3c0b752ac5c32ae117669e63c57bfb55e76f0df
tai.truong <tai.truong@sotatek.com>
2023-05-25 10:13:09 +0700

    fix: add auth when check bookmark

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,6 +10,7 @@ import { getAllBookmarks } from "~/commons/utils/userRequest";
 import { async } from "q";
 import { NETWORK, NETWORK_TYPES } from "~/commons/utils/constants";
 import SyncBookmarkModal from "~/components/commons/Layout/Header/SyncBookmarkModal";
+import useAuth from "~/commons/hooks/useAuth";
 
 const HomeContainer = styled(Container)`
   padding-top: 30px;
@@ -19,20 +20,23 @@ const HomeContainer = styled(Container)`
 const Home = () => {
   const [, setBookmark] = useLocalStorage<Bookmark[]>("bookmark", []);
   const [openSyncBookmark, setOpenSyncBookmark] = useState(false);
+  const { isLoggedIn } = useAuth();
   useEffect(() => {
     document.title = `Cardano Explorer`;
   }, []);
 
   useAsync(async () => {
-    if (
-      (((JSON.parse(localStorage.getItem("bookmark") || "") as Bookmark[]) || [])?.filter((r) => !r.id) || []).length >
-      0
-    ) {
-      setOpenSyncBookmark(true);
-    } else {
-      const { data } = await getAllBookmarks(NETWORK_TYPES[NETWORK]);
-      if (data) {
-        setBookmark(data);
+    if (isLoggedIn) {
+      if (
+        (((JSON.parse(localStorage.getItem("bookmark") || "") as Bookmark[]) || [])?.filter((r) => !r.id) || [])
+          .length > 0
+      ) {
+        setOpenSyncBookmark(true);
+      } else {
+        const { data } = await getAllBookmarks(NETWORK_TYPES[NETWORK]);
+        if (data) {
+          setBookmark(data);
+        }
       }
     }
   }, []);
