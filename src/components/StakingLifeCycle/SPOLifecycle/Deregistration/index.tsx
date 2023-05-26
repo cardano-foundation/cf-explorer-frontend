e1194e43be7b227383b48e82730b9d362a229389
tung.pham <pvtung.197@gmail.com>
2023-05-26 15:53:47 +0700

    bugfix:finding-29 update deregistration timeline when poolHold Null

--- src/components/StakingLifeCycle/SPOLifecycle/Deregistration/index.tsx
+++ src/components/StakingLifeCycle/SPOLifecycle/Deregistration/index.tsx
@@ -32,6 +32,7 @@ import FeeBoxSPO from "~/components/commons/FeeBoxSPO";
 import CardanoSystem from "~/components/commons/CardanoSystem";
 import { LineArrowItem } from "~/components/commons/LineArrow";
 import DrawPath from "~/components/commons/DrawPath";
+import { AditionalLabel } from "./RecentDeregistrations/styles";
 
 const Deregistration = () => {
   const [selected, setSelected] = useState<SPODeregistration | null>(null);
@@ -76,18 +77,80 @@ const DeregistrationTimeline = ({ selected, toggleModal, showBackButton }: Dereg
   };
 
   const { sidebar } = useSelector(({ user }: RootState) => user);
-
+  const isPoolHold = !!selected?.poolHold;
   const paths = useMemo((): LineArrowItem[] => {
+    if (isPoolHold)
+      return [
+        {
+          start: SPOHolderRef,
+          startPosition: { 0: ["center", "bottom"], lg: ["right", "middle"] },
+          end: cadarnoSystemRef,
+          endPosition: { 0: ["center", "top"], lg: ["left", "middle"] },
+          startOffset: { 0: [0], lg: [-10, -15] },
+          endOffset: { 0: [0, 0], lg: [0] },
+          arrow: { 0: "top", lg: "left" },
+          fold: { 0: "none" }
+        },
+        {
+          start: SPOHolderRef,
+          startPosition: { 0: ["right", "middle"], lg: ["center", "bottom"] },
+          end: deregistrationRef,
+          endPosition: { 0: ["center", "top"], lg: ["left", "middle"] },
+          startOffset: { 0: [0], lg: [0, 0] },
+          endOffset: { 0: [0, 30], lg: [0] },
+          arrow: { 0: "top", lg: "left" },
+          fold: { 0: "horizontal", lg: "vertical" }
+        },
+        {
+          start: deregistrationRef,
+          startPosition: { 0: ["center", "bottom"], lg: ["right", "middle"] },
+          end: cadarnoSystemRef,
+          endPosition: { 0: ["center", "top"], sm: ["left", "middle"], lg: ["center", "bottom"] },
+          startOffset: { 0: [0], lg: [0, 0] },
+          endOffset: { 0: [-94, 45], sm: [10, 0], lg: [0] },
+          arrow: { 0: "top", sm: "left", lg: "bottom" },
+          fold: { 0: "none", sm: "vertical", lg: "horizontal" }
+        },
+        {
+          start: cadarnoSystemRef,
+          startPosition: { 0: ["right", "middle"], lg: ["left", "middle"] },
+          end: holdRef,
+          endPosition: { 0: ["center", "bottom"], lg: ["right", "middle"] },
+          startOffset: { 0: [-10], lg: [50, -70] },
+          endOffset: { 0: [50, -10], sm: [0, -10], lg: [0] },
+          arrow: { 0: "bottom", lg: "right" },
+          fold: { 0: "horizontal", lg: "none" }
+        },
+        {
+          start: holdRef,
+          startPosition: { 0: ["center", "top"], lg: ["left", "middle"] },
+          end: SPOHolderRef,
+          endPosition: { 0: ["right", "middle"], lg: ["right", "middle"] },
+          startOffset: { 0: [50], sm: [0], lg: [0] },
+          endOffset: { 0: [0, 0], lg: [-5, -84] },
+          arrow: { 0: "right", lg: "right" },
+          fold: { 0: "vertical", lg: "none" }
+        }
+      ];
     return [
       {
         start: SPOHolderRef,
+        startPosition: { 0: ["right", "bottom"], sm: ["right", "middle"], lg: ["right", "middle"] },
+        end: feeRef,
+        endPosition: { 0: ["center", "top"], lg: ["left", "middle"] },
+        startOffset: { 0: [-5, -50], sm: [0], lg: [-10, -15] },
+        endOffset: { 0: [0, 0], lg: [0] },
+        fold: { 0: "none", sm: "horizontal", lg: "none" }
+      },
+      {
+        start: feeRef,
         startPosition: { 0: ["center", "bottom"], lg: ["right", "middle"] },
         end: cadarnoSystemRef,
-        endPosition: { 0: ["center", "top"], lg: ["left", "middle"] },
-        startOffset: { 0: [0], lg: [-10, -15] },
-        endOffset: { 0: [0, 0], lg: [0] },
-        arrow: { 0: "top", lg: "left" },
-        fold: { 0: "none" }
+        endPosition: { 0: ["right", "top"], sm: ["right", "middle"], lg: ["left", "middle"] },
+        startOffset: { 0: [0] },
+        endOffset: { 0: [-22, 45], sm: [-10, 0], lg: [10, 0] },
+        arrow: { 0: "top", sm: "right", lg: "left" },
+        fold: { 0: "none", sm: "vertical", lg: "none" }
       },
       {
         start: SPOHolderRef,
@@ -108,29 +171,9 @@ const DeregistrationTimeline = ({ selected, toggleModal, showBackButton }: Dereg
         endOffset: { 0: [-94, 45], sm: [10, 0], lg: [0] },
         arrow: { 0: "top", sm: "left", lg: "bottom" },
         fold: { 0: "none", sm: "vertical", lg: "horizontal" }
-      },
-      {
-        start: cadarnoSystemRef,
-        startPosition: { 0: ["right", "middle"], lg: ["left", "middle"] },
-        end: holdRef,
-        endPosition: { 0: ["center", "bottom"], lg: ["right", "middle"] },
-        startOffset: { 0: [-10], lg: [50, -70] },
-        endOffset: { 0: [50, -10], sm: [0, -10], lg: [0] },
-        arrow: { 0: "bottom", lg: "right" },
-        fold: { 0: "horizontal", lg: "none" }
-      },
-      {
-        start: holdRef,
-        startPosition: { 0: ["center", "top"], lg: ["left", "middle"] },
-        end: SPOHolderRef,
-        endPosition: { 0: ["right", "middle"], lg: ["right", "middle"] },
-        startOffset: { 0: [50], sm: [0], lg: [0] },
-        endOffset: { 0: [0, 0], lg: [-5, -84] },
-        arrow: { 0: "right", lg: "right" },
-        fold: { 0: "vertical", lg: "none" }
       }
     ];
-  }, []);
+  }, [selected]);
 
   return (
     <Box>
@@ -169,17 +212,19 @@ const DeregistrationTimeline = ({ selected, toggleModal, showBackButton }: Dereg
           ref={SPOHolderRef}
           data={{ poolName: selected?.poolName, poolView: selected?.poolView, stakeKeys: selected?.stakeKeys }}
         />
-        <MiddleGroup sidebar={+sidebar}>
+        <MiddleGroup sidebar={+sidebar} hold={+isPoolHold}>
           <BoxGroup sidebar={+sidebar}>
-            <Box>
-              <HoldBoxSPO
-                sidebar={sidebar}
-                ref={holdRef}
-                value={selected?.poolHold || ""}
-                txHash={selected?.txHash || ""}
-              />
-            </Box>
-            <Box>
+            {isPoolHold && (
+              <Box>
+                <HoldBoxSPO
+                  sidebar={sidebar}
+                  ref={holdRef}
+                  value={selected?.poolHold || ""}
+                  txHash={selected?.txHash || ""}
+                />
+              </Box>
+            )}
+            <Box display='flex' justifyContent={isPoolHold ? "unset" : "center"}>
               <FeeBoxSPO sidebar={sidebar} ref={feeRef} value={selected?.fee || ""} txHash={selected?.txHash || ""} />
             </Box>
           </BoxGroup>
@@ -188,9 +233,14 @@ const DeregistrationTimeline = ({ selected, toggleModal, showBackButton }: Dereg
           </StyledCertificateShape>
         </MiddleGroup>
         <CardanoSystem ref={cadarnoSystemRef} />
-
         <DrawPath paths={paths} />
       </DrawContainer>
+      {!isPoolHold && (
+        <AditionalLabel>
+          Pool hold paid during registration is refunded 1 epoch after the retirement epoch to the reward address of the
+          pool operator
+        </AditionalLabel>
+      )}
     </Box>
   );
 };
