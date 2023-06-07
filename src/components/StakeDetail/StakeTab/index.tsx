e02f41a41348e67456d283b258852ec21a89895c
Sotatek-HuyDoan <132533358+Sotatek-HuyDoan@users.noreply.github.com>
2023-06-07 08:46:31 +0700

    fix: MET-615-missing-scroll-bar
    Missing scroll bar

--- src/components/StakeDetail/StakeTab/index.tsx
+++ src/components/StakeDetail/StakeTab/index.tsx
@@ -76,6 +76,7 @@ const StakeTab = () => {
             onChange={handleChange}
             TabIndicatorProps={{ style: { background: theme.palette.primary.main } }}
             variant="scrollable"
+            visibleScrollbar
           >
             {tabs.map(({ icon: Icon, key, label }) => (
               <Tab
