69cf5e62bf0f348bc46836aa93395e45ae5e959c
Sotatek-TungPham <tung.pham@sotatek.com>
2023-06-08 15:49:13 +0700

    test:MET-1175 [UT] Update Unit test for PolicyDetail

---
+++ src/components/PolicyDetail/PolicyOverview/PolicyOverview.test.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import { truncateCustom } from "src/commons/utils/helper";
+import { useScreen } from "src/commons/hooks/useScreen";
+
+import { render } from "../../../test-utils";
+
+import PolicyOverview from "./index";
+
+jest.mock("src/commons/hooks/useScreen", () => {
+  return {
+    __esModule: true,
+    ...jest.requireActual("src/commons/hooks/useScreen"),
+    useScreen: jest.fn()
+  };
+});
+
+const mockData = {
+  policyId: "a0028f350aaabe0545fdcb56b039bfb08e4bb4d8c4d7c3c7d481c235"
+} as PolicyDetail;
+describe("PolicyOverview", () => {
+  const mockUseScreen = useScreen as jest.Mock;
+
+  it("testing on mobile and tablet devices", async () => {
+    mockUseScreen.mockReturnValue({ isMobile: true, isTablet: true });
+    render(<PolicyOverview data={mockData} loading={false} />);
+    const policyIdElement = screen.getByText(truncateCustom(mockData.policyId, 5, 5), { exact: false });
+    expect(policyIdElement).toBeInTheDocument();
+  });
+
+  it("testing rendering policy details", () => {
+    mockUseScreen.mockReturnValue({ isMobile: false, isTablet: false });
+    render(<PolicyOverview data={mockData} loading={false} />);
+    expect(screen.getByText("Policy Details")).toBeInTheDocument();
+    expect(screen.getByText("Policy ID:")).toBeInTheDocument();
+    expect(screen.getByText(mockData.policyId)).toBeInTheDocument();
+  });
+
+  it("testing loadidng state", () => {
+    mockUseScreen.mockReturnValue({ isMobile: false, isTablet: false });
+    render(<PolicyOverview data={mockData} loading={true} />);
+
+    const loadingElement = screen.getByTestId("loading-element");
+    expect(loadingElement).toBeInTheDocument();
+  });
+
+  it("testing opening modal", async () => {
+    mockUseScreen.mockReturnValue({ isMobile: false, isTablet: false });
+    render(<PolicyOverview data={mockData} loading={false} />);
+    const button = screen.getByRole("button", {
+      name: /policy script/i
+    });
+
+    userEvent.click(button);
+    const modalEl = await screen.findByTestId("modal-testid");
+    expect(modalEl).toBeInTheDocument();
+  });
+});
