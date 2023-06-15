ba1af3cf18d0e9ec71e9a45e247cb3c4800bb197
trung.hoang <trung.hoang@sotatek.com>
2023-06-15 16:44:10 +0700

    chor: add filter stake keys (#648)

--- src/components/commons/Layout/Header/HeaderSearch/index.tsx
+++ src/components/commons/Layout/Header/HeaderSearch/index.tsx
@@ -68,6 +68,12 @@ const options: Option[] = [
     detail: details.token
   },
   {
+    value: "stakes",
+    label: "Stake keys",
+    paths: [routers.STAKE_DETAIL, routers.STAKE_DETAIL],
+    detail: details.stake
+  },
+  {
     value: "addresses",
     label: "Addresses",
     paths: [
