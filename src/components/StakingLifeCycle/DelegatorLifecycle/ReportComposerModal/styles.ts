4cdd47272c05d9053c58d0ab895f4fffc61ca499
Sotatek-TungPham <tung.pham@sotatek.com>
2023-06-21 09:15:17 +0700

    test:MET-1180 unit test for delegator lifecycle - report composer modal

--- src/components/StakingLifeCycle/DelegatorLifecycle/ReportComposerModal/styles.ts
+++ src/components/StakingLifeCycle/DelegatorLifecycle/ReportComposerModal/styles.ts
@@ -145,7 +145,7 @@ export const TextRequired = styled("div")`
   line-height: 22px;
 `;
 
-export const ButtonEvent = styled(Button)<{ active: boolean }>(({ theme, active }) => ({
+export const ButtonEvent = styled(Button)<{ active: number }>(({ theme, active }) => ({
   background: active ? "#667085" : "#f2f2f2",
   color: active ? "#fff" : "#667085",
   borderRadius: "6px",
