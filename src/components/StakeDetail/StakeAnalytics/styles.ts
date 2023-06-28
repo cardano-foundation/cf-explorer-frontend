d4216f3c855922518b4e6a9c950bc13ee0aa5661
Truong Manh Nguyen (truong.nguyen4) <truong.nguyen4@sotatek.com>
2023-06-28 13:55:48 +0700

    Fix degrade case mobile

--- src/components/StakeDetail/StakeAnalytics/styles.ts
+++ src/components/StakeDetail/StakeAnalytics/styles.ts
@@ -180,9 +180,11 @@ export const Tab = styled(Button)<{ active: number }>(({ theme, active }) => ({
     color: active ? `${theme.palette.text.dark} !important` : theme.palette.grey[400]
   },
   [theme.breakpoints.down("lg")]: {
+    backgroundColor: active ? `${theme.palette.primary.main} !important` : "none",
+    color: active ? `${theme.palette.primary.contrastText} !important` : theme.palette.grey[400],
     "&:hover": {
       backgroundColor: theme.palette.primary.main,
-      color: theme.palette.primary.contrastText
+      color: active ? `${theme.palette.primary.contrastText} !important` : theme.palette.grey[400]
     }
   },
   [theme.breakpoints.down("sm")]: {
