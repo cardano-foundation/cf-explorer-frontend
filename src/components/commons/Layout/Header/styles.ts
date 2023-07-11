b3ae8c04f1d411baccd2386b5907da8b3d9f4d95
huy.nguyen <huy.nguyen8@sotatek.com>
2023-07-11 15:43:32 +0700

    fix: MET-615-pool-detail-button-back-lie-on-top-dropdown-search

--- src/components/commons/Layout/Header/styles.ts
+++ src/components/commons/Layout/Header/styles.ts
@@ -48,10 +48,6 @@ export const HeaderMain = styled("div")<{ home: number }>(({ theme, home }) => (
   position: "relative",
   textAlign: "start",
   padding: home ? "0px 0px 50px" : "27px 0px",
-  "& > div": {
-    paddingTop: home ? "0px" : "30px",
-    marginBottom: home ? "0px" : "calc(-25px - 1.5715rem)"
-  },
   [theme.breakpoints.down("md")]: {
     padding: home ? "62px 0px 48px" : 0,
     display: home ? "block" : "none"
