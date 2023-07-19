3300fabde4126e79869e2d198252b7a4ce1685b7
trung.hoang <trung.hoang@sotatek.com>
2023-07-19 17:03:32 +0700

    fix: MET-1373 grammar errors

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -8,7 +8,7 @@ import useToast from "src/commons/hooks/useToast";
 import { HideIcon, LockIcon, ShowIcon } from "src/commons/resources";
 import { routers } from "src/commons/routers";
 import { NETWORK, NETWORK_TYPES } from "src/commons/utils/constants";
-import { removeAuthInfo } from "src/commons/utils/helper";
+import { isValidEmail, removeAuthInfo } from "src/commons/utils/helper";
 import { getInfo, signIn } from "src/commons/utils/userRequest";
 import ConnectWallet from "src/components/commons/Layout/Header/ConnectWallet";
 import { setUserData } from "src/stores/user";
@@ -91,7 +91,11 @@ export default function SignIn() {
   };
 
   const handleRedirectBack = () => {
-    if (history.length > 1 && !AUTHENTICATE_ROUTES.includes(history.location.pathname) && history.location.pathname !== routers.HOME) {
+    if (
+      history.length > 1 &&
+      !AUTHENTICATE_ROUTES.includes(history.location.pathname) &&
+      history.location.pathname !== routers.HOME
+    ) {
       history.goBack();
     } else {
       history.replace(routers.HOME);
@@ -99,7 +103,7 @@ export default function SignIn() {
   };
 
   const handleLoginSuccess = () => {
-    toast.success("Login success");
+    toast.success("You are now signed in");
     handleRedirectBack();
   };
 
@@ -128,9 +132,12 @@ export default function SignIn() {
   const getError = (name: string, value: string) => {
     let error = "";
     switch (name) {
-      case "username":
+      case "email":
         if (!value) {
           error = "Please enter Email Address";
+        }
+        if (!isValidEmail(value)) {
+          error = "Please enter a valid email address";
         }
         break;
       case "password":
@@ -219,7 +226,9 @@ export default function SignIn() {
             </CloseButton>
             {invalidInfomation ? (
               <Box pt={"24px"}>
-                <AlertCustom severity="error">Incorrect email address or password</AlertCustom>
+                <AlertCustom severity="error">
+                  Unable to sign in. Please check your email address and password.
+                </AlertCustom>
               </Box>
             ) : null}
             <WrapInput>
