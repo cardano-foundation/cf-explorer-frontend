5d33530d14d78ed5e8995e05f8d09b8578d03afc
huy.nguyen <huy.nguyen8@sotatek.com>
2023-07-21 09:48:33 +0700

    fix: MET-1437-Page-level-Container-width-should-be-responsive-enough-to-see-full-width

--- src/components/commons/Table/index.tsx
+++ src/components/commons/Table/index.tsx
@@ -407,7 +407,6 @@ const Table: React.FC<TableProps> = ({
         ref={wrapperRef}
         maxHeight={maxHeight}
         minHeight={(!data || data.length === 0) && !loading ? 360 : loading ? 400 : 150}
-        height={heightTable}
         className={data && data.length !== 0 ? "table-wrapper" : "hide-scroll"}
         loading={loading ? 1 : 0}
       >
