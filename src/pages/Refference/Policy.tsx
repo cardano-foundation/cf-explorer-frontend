d8f58a857caae67c11dbed7c7f3a5ea786363ce7
trung.hoang <trung.hoang@sotatek.com>
2023-07-21 09:35:48 +0700

    feature: MET-1450-1451-1466 add faq, terms, policy

---
+++ src/pages/Refference/Policy.tsx
@@ -0,0 +1,249 @@
+import { IRisLogo } from "src/commons/resources";
+
+import { ContainerReffer, TextReffer, Content, TextHeader } from "./styles";
+
+const Policy = () => {
+  return (
+    <ContainerReffer>
+      <IRisLogo />
+      <Content>
+        <TextHeader>Privacy Policy - Iris.cardano.org</TextHeader>
+
+        <TextReffer>PLEASE READ THE PRIVACY POLICY CAREFULLY.</TextReffer>
+
+        <TextReffer>
+          Cardano Foundation (referred to as the &ldquo;Company&rdquo;, , &ldquo;we&rdquo;, &ldquo;our&rdquo; or
+          &ldquo;us&rdquo;) is committed to the protection of your Personal Data and takes the matter of protecting your
+          privacy as high priority.
+        </TextReffer>
+
+        <TextReffer>
+          1. TYPES OF DATA WE COLLECT The types of Personal Data that we collect directly from you or from third parties
+          depend on the circumstances of collection and on the nature of the service requested or transaction
+          undertaken. It may include (but is not limited to):
+        </TextReffer>
+
+        <TextReffer>
+          (a) personal information that links back to an individual, e.g., name, gender, date of birth, and other
+          personal identification numbers; (b) contact information, e.g., address, phone number and email address; (c)
+          technical information, e.g., IP address for API services and login; (d) statistical data, e.g., hits to
+          website. This Privacy Policy covers the information we collect about you when you use our products or
+          services, or otherwise interact with iris.cardano.org/, unless a different privacy policy is displayed. This
+          policy also explains your choices about how we use information about you.
+        </TextReffer>
+
+        <TextReffer>
+          Your choices include how you can object to certain uses of information about you and how you can access and
+          update certain information about you. If you do not agree to the terms of this Policy, please do not use the
+          Site, or any of our Services. Each time you use any Site, or any Services, the current version of this Privacy
+          Policy will apply.
+        </TextReffer>
+
+        <TextReffer>
+          2. HOW DO WE COLLECT PERSONAL DATA? This Privacy Policy covers any Personal Data provided to us:
+        </TextReffer>
+
+        <TextReffer>
+          (a) when you engage with our products and services; (b) when you create an account with us; (c) under any
+          other contractual agreement or arrangement. Some of the other ways we may collect Personal Data shall include
+          (but is not limited to):
+        </TextReffer>
+
+        <TextReffer>
+          (a) communications with you via telephone, letter, fax and email; (b) when you visit our website; (c) when you
+          contact us in person; (d) when we contact you in person; (e) when we collect information about you from third
+          parties; and other channels including our support.
+        </TextReffer>
+
+        <TextReffer>
+          3. HOW DO WE COLLECT YOUR PERSONAL DATA ON OUR WEBSITE? From our website, we collect your Personal Data in the
+          following ways:
+        </TextReffer>
+
+        <TextReffer>
+          (a) IP address We use your IP address to help diagnose problems with our server, and to administer our
+          website.
+        </TextReffer>
+
+        <TextReffer>
+          (b) Cookies A cookie is an element of data that a website can send to your browser, which may then store it on
+          your system. We use cookies in some of our pages to store your preferences and record session information.
+        </TextReffer>
+
+        <TextReffer>
+          The information that we collect is then used to ensure a more personalized service level for our users. You
+          can adjust settings on your browser so that you will be notified when you receive a cookie. Please refer to
+          your browser documentation to check if cookies have been enabled on your computer or to request not to receive
+          cookies.
+        </TextReffer>
+
+        <TextReffer>
+          As cookies allow you to take advantage of some of the Website&rsquo;s essential features, we recommend that
+          you accept cookies. For instance, if you block or otherwise reject our cookies, you will not be able to use
+          any products or services on the website that may require you to log-in (token holdings store cookies for
+          favorite).
+        </TextReffer>
+
+        <TextReffer>
+          It is important that you prevent unauthorized access to your password and your computer. You should always log
+          out after using a shared computer. Information collected from cookies is used by us to evaluate the
+          effectiveness of our site, analyze trends, and manage the platform. The information collected from cookies
+          allows us to determine such things as which parts of our site are most visited and difficulties our visitors
+          may experience in accessing our site.
+        </TextReffer>
+
+        <TextReffer>
+          With this knowledge, we can improve the quality of your experience on the platform by recognizing and
+          delivering more of the most desired features and information, as well as by resolving access difficulties. We
+          also use cookies and/or a technology known as web bugs or clear gifs, which are typically stored in emails to
+          help us confirm your receipt of, and response to our emails and to provide you with a more personalized
+          experience when using our site. Your continued use of this site, as well as any subsequent usage, will be
+          interpreted as your consent to cookies being stored on your device.
+        </TextReffer>
+
+        <TextReffer>
+          (c) User feedback form Our feedback form requires you to give us contact information (e.g. your name and email
+          address) so that we can respond to your comments. We use your contact information from the registration form
+          to send you information about our company. Your contact information is also used to contact you where
+          necessary.
+        </TextReffer>
+
+        <TextReffer>
+          (d) General Site tracking We also use third party service provider(s), to assist us in better understanding
+          the use of our site. Our service provider(s) will place cookies on the hard drive of your computer and will
+          receive information that we select, for example, how visitors navigate around our site, what pages are browsed
+          and general transaction information. Our service provider(s) analyzes this information and provides us with
+          aggregate reports.
+        </TextReffer>
+
+        <TextReffer>
+          The information and analysis provided by our service provider(s) will be used to assist us in better
+          understanding our visitors' interests in our site and how to better serve those interests. The information
+          collected by our service provider(s) may be linked to and combined with information that we collect about you
+          while you are using the platform. Our service provider(s) is/are contractually restricted from using
+          information they receive from our Site other than to assist us.
+        </TextReffer>
+
+        <TextReffer>
+          (e) Web Server site visits logging The following is how we store the web server site visit logs (applicable to
+          https://iris.cardano.org/):
+        </TextReffer>
+
+        <TextReffer>
+          (i) To throttle the rate of requests and prevent certain types of attacks against us, we track the incoming IP
+          addresses for very short periods of time and is then released. (ii) By default, we do NOT store identifiable
+          'x-forwarded-for' originating IPs during your site visit in the Web Server site visits logs. (iii) However in
+          the event of certain types of third-party attacks, general server/application troubleshooting or other related
+          reasons, we might temporarily activate the 'x-forwarded-for' logging. (iv) As part of our routine server
+          maintenance, all raw web server site visit logs are retained for a minimum of 5 days only and then purged on
+          an automated scheduled basis.
+        </TextReffer>
+
+        <TextReffer>
+          4.WHAT DO WE USE YOUR PERSONAL DATA FOR? We may use your Personal Data for the following purposes:
+        </TextReffer>
+
+        <TextReffer>
+          (a) to enable us to provide our services and perform our services to you; (b) to protect the safety and well
+          being of yourself and/or other customers; (c) to investigate and respond to claims and inquiries from you; (d)
+          for business development purposes such as statistical and marketing analysis, systems testing, maintenance and
+          development, customer surveys or to help us in any future dealings with you, for example by identifying your
+          requirements and preference; (e) to comply with any legal or regulatory requirements; and/ or (f) for all
+          other purposes ancillary to any of the purposes stated above. (&quot;Core Purposes&quot;) (g) to communicate
+          offers, product, services and information on products and activities; (h) marketing/cross-marketing and
+          communicating with you in relation to products and services offered by us and our service partners as well as
+          our appointed agents; and/or (i) for all other purposes ancillary to any of the purposes stated above.
+          (&quot;Ancillary Purposes&quot;) (collectively, &quot;Purposes&quot;)
+        </TextReffer>
+
+        <TextReffer>
+          5. ACCESSING / CORRECTING / UPDATING YOUR PERSONAL DATA You may request to obtain information of your personal
+          data and also update or make amendments to your personal data as below: (a) for online registered customers,
+          you may login to your online account and update your personal data. We will endeavour to provide the
+          information back to you as soon as practicable. However we also reserve the right to validate all requests for
+          the authenticity of the request.
+        </TextReffer>
+
+        <TextReffer>
+          6. WITHDRAWING CONSENT Please note that it is obligatory for the Company to process your Personal Data for the
+          Core Purpose as stated above, without which some services or features provided by Iris.cardano.org may be
+          affected.
+        </TextReffer>
+
+        <TextReffer>
+          If we do not have your consent to process your Personal Data for the Ancillary Purposes, we will not be able
+          to keep you updated about our future, new and/or enhanced services and products. Nevertheless, you may stop
+          receiving promotional activities by:
+        </TextReffer>
+
+        <TextReffer>
+          (a) unsubscribing from the mailing list; (b) editing the relevant account settings to unsubscribe; or (c)
+          sending a request via gdpr@cardanofoundation.org
+        </TextReffer>
+
+        <TextReffer>
+          7. TO WHOM DO WE DISCLOSE YOUR PERSONAL DATA? We will not trade or sell your Personal Data to third parties.
+          Your Personal Data shall only be disclosed or transferred to the following third parties appointed or
+          authorised by the Company for the fulfilment of the Purpose of: (a) data warehouses; (b) IT service providers;
+          (c) data analytics and/or marketing agency; (d) legal bodies as permitted or required by law such as in
+          compliance with a warrant or subpoena issued by a court of competent jurisdiction; and/or (e) regulatory
+          authorities applicable to you; and/or (f) safety and security personnel.
+        </TextReffer>
+
+        <TextReffer>
+          In addition to the above, your Personal Data may also be disclosed or transferred to any of the
+          Company&rsquo;s actual and potential assignee, transferee or acquirer (including our affiliates and
+          subsidiaries) or our business, assets or group companies, or in connection with any corporate restructuring or
+          exercise including the our restructuring to transfer the business, assets and/or liabilities.
+        </TextReffer>
+
+        <TextReffer>
+          We shall take practical steps to ensure that their employees, officers, agents, consultants, contractors and
+          such other third parties mentioned above who are involved in the collection, use and disclosure of your
+          Personal Data will observe and adhere to the terms of this Privacy Statement.
+        </TextReffer>
+
+        <TextReffer>
+          We are subject to various legal and regulatory obligations imposed by the laws and supervisory authorities of
+          various jurisdictions e.g., anti-money laundering laws, anti-terrorism financing, financial services laws,
+          corporation laws and privacy laws. These obligations may require us to process certain data for payment
+          processing, compliance with court orders or other purposes not disclosed herein.
+        </TextReffer>
+
+        <TextReffer>
+          8. HOW LONG WILL WE RETAIN YOUR PERSONAL DATA? The Company stores data in Amazon Web Services What hosting
+          provider with servers across regions and we shall take all reasonable steps to ensure that all Personal Data
+          is destroyed or permanently deleted when no longer required for the Purpose and prepare a disposal schedule
+          for inactive data after 24 month period.
+        </TextReffer>
+
+        <TextReffer>
+          9. LINKS TO THIRD PARTY WEBSITES We may link this website and/or our applications to other companies&rsquo; or
+          organizations&rsquo; websites (collectively, &ldquo;Third Party Sites&rdquo;). This Privacy Notice does not
+          apply to such Third Party Sites as those sites are outside our control. If you access Third Party Sites using
+          the links provided, the operators of these sites may collect your personal information.
+        </TextReffer>
+
+        <TextReffer>
+          Please ensure that you are satisfied with the privacy statements of these Third Party Sites before you submit
+          any personal information. We try, as far as we can, to ensure that all third party linked sites have
+          equivalent measures for protection of your personal information, but we cannot be held responsible legally or
+          otherwise for the activities, privacy policies or levels of privacy compliance of these Third Party Sites.
+        </TextReffer>
+
+        <TextReffer>
+          10. ADDITIONAL INFORMATION OR ASSISTANCE Please note that this Privacy Statement may be amended from time to
+          time in accordance to applicable laws and regulations and such variations may be applicable to you.
+        </TextReffer>
+
+        <TextReffer>
+          For further inquiries or complaints in relation to our handling of your Personal Data or our Privacy Policy or
+          wish to access, update or amend your Personal Data as mentioned above please contact us via our feedback form
+          at gdpr@cardanofoundation.org.
+        </TextReffer>
+      </Content>
+    </ContainerReffer>
+  );
+};
+
+export default Policy;
