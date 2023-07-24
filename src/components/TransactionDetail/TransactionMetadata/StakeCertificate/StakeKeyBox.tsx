b34b8486d53bafde58299aeed4111d9f52dc3d3a
huy.nguyen <huy.nguyen8@sotatek.com>
2023-07-24 17:12:49 +0700

    fix: MET-615-Transaction-detail-change-text

--- src/components/TransactionDetail/TransactionMetadata/StakeCertificate/StakeKeyBox.tsx
+++ src/components/TransactionDetail/TransactionMetadata/StakeCertificate/StakeKeyBox.tsx
@@ -14,7 +14,7 @@ type TProps = {
 const StakeKeyBox = ({ data }: TProps) => {
   const leftRow = [
     {
-      label: "Address Stake Key",
+      label: "Stake Address",
       value: data.stakeAddress,
       originValue: data.stakeAddress
     }
