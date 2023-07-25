bf4cb9bb93a4593aa92283d1427ac548ca4df4dd
david.nguyen4 <david.nguyen4@sotatek.com>
2023-07-25 14:15:01 +0700

    chore: block list column name

--- cypress/e2e/blocks.cy.ts
+++ cypress/e2e/blocks.cy.ts
@@ -17,4 +17,14 @@ describe("template spec", () => {
     cy.visit("/block/7588518");
     cy.get('[data-testid="block-details-total-output-in-ada"]').should("contain", "18,246,431.529342");
   });
+  it.only("should have enough column", () => {
+    cy.visit("/blocks");
+    cy.get('.css-1dz0v3k > tr > :nth-child(1)').contains("Block No");
+    cy.get('.css-1dz0v3k > tr > :nth-child(2)').contains("Block ID");
+    cy.get('.css-1dz0v3k > tr > :nth-child(3)').contains("Epoch/Slot");
+    cy.get('.css-1dz0v3k > tr > :nth-child(4)').contains("Created At");
+    cy.get('.css-1dz0v3k > tr > :nth-child(5)').contains("Transactions");
+    cy.get('.css-1dz0v3k > tr > :nth-child(6)').contains("Fees");
+    cy.get('.css-1dz0v3k > tr > :nth-child(7)').contains("Output");
+  });
 });
