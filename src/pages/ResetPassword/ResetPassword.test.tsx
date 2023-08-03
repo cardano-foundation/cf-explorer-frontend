e7804a1530f2c519b88b512d99c7689c85287264
Trung Hoang <92282849+Sotatek-TrungHoang@users.noreply.github.com>
2023-08-03 14:48:57 +0700

    Bugfix/unit test (#1549)
    * fix(tests): Layout.test.tsx
    
    * fix(tests): DelegationOverview.test.tsx
    
    * fix(tests): ContractDetailContent.test.tsx
    
    * fix(tests): DetailViewToken.test.tsx
    
    * fix(tests): TopDelegators.test.tsx
    
    * fix(tests): TransactionChart.test.tsx
    
    * fix(tests): Statistic.test.tsx
    
    * fix(tests): TokenAutocomplete.test.tsx
    
    * fix(tests): ToastContainer.test.tsx
    
    * fix(tests): Delegations.test.tsx
    
    * fix(tests): DetailViewContractHash.test.tsx
    
    * fix: update unit test
    
    * fix(tests): Home.test.tsx
    
    * fix(tests): ContractDetail.test.tsx
    
    * chore: fix unit jest test
    
    * fix(tests): ReportDetailStakeyTabs.test.tsx
    
    * fix(tests): TokenTransaction.test.tsx
    
    * fix: env default for testing failed
    
    ---------
    
    Co-authored-by: ngoc.tran3 <ngoc.tran3@sotatek.com>
    Co-authored-by: Truong Manh Nguyen (truong.nguyen4) <truong.nguyen4@sotatek.com>

--- src/pages/ResetPassword/ResetPassword.test.tsx
+++ src/pages/ResetPassword/ResetPassword.test.tsx
@@ -9,6 +9,11 @@ jest.mock("src/commons/utils/axios", () => ({
           code: "SS_0"
         }
       });
+    },
+    get: () => {
+      return Promise.resolve({
+        data: true
+      });
     }
   }
 }));
@@ -17,21 +22,25 @@ describe("ResetPassword page", () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
-  it("should component render", () => {
-    render(<ResetPassword />);
-    expect(screen.getByText(/reset password/i)).toBeInTheDocument();
-    expect(screen.getByPlaceholderText(/new password/i)).toBeInTheDocument();
-    expect(screen.getByText(/confirm new password/i)).toBeInTheDocument();
-    expect(screen.getByPlaceholderText(/confirm password/i)).toBeInTheDocument();
-    expect(screen.getByRole("button", { name: /submit/i })).toBeInTheDocument();
+  it("should component render", async () => {
+    render(<ResetPassword codeVerify="123123" />);
+    await waitFor(() => {
+      expect(screen.getByText(/reset password/i)).toBeInTheDocument();
+      expect(screen.getByPlaceholderText(/new password/i)).toBeInTheDocument();
+      expect(screen.getByText(/confirm new password/i)).toBeInTheDocument();
+      expect(screen.getByPlaceholderText(/confirm password/i)).toBeInTheDocument();
+      expect(screen.getByRole("button", { name: /submit/i })).toBeInTheDocument();
+    });
   });
 
   it("should user submit the form", async () => {
     const password = "Admin@123456";
-    render(<ResetPassword />);
-    fireEvent.change(screen.getByPlaceholderText(/new password/i), { target: { value: password } });
-    fireEvent.change(screen.getByPlaceholderText(/confirm password/i), { target: { value: password } });
-    fireEvent.click(screen.getByRole("button", { name: /submit/i }));
+    render(<ResetPassword codeVerify="123123" />);
+    await waitFor(() => {
+      fireEvent.change(screen.getByPlaceholderText(/new password/i), { target: { value: password } });
+      fireEvent.change(screen.getByPlaceholderText(/confirm password/i), { target: { value: password } });
+      fireEvent.click(screen.getByRole("button", { name: /submit/i }));
+    });
     await waitFor(() => {
       expect(screen.getByText("Your password has been reset successfully")).toBeInTheDocument();
     });
@@ -39,10 +48,12 @@ describe("ResetPassword page", () => {
 
   it("should user submit invalid password", async () => {
     const invalidPassword = "123456";
-    render(<ResetPassword />);
-    fireEvent.change(screen.getByPlaceholderText(/new password/i), { target: { value: invalidPassword } });
-    fireEvent.change(screen.getByPlaceholderText(/confirm password/i), { target: { value: invalidPassword } });
-    fireEvent.click(screen.getByRole("button", { name: /submit/i }));
+    render(<ResetPassword codeVerify="123123" />);
+    await waitFor(() => {
+      fireEvent.change(screen.getByPlaceholderText(/new password/i), { target: { value: invalidPassword } });
+      fireEvent.change(screen.getByPlaceholderText(/confirm password/i), { target: { value: invalidPassword } });
+      fireEvent.click(screen.getByRole("button", { name: /submit/i }));
+    });
     await waitFor(() => {
       expect(screen.getByText(/password has to be from 8 to 30/i)).toBeInTheDocument();
     });
