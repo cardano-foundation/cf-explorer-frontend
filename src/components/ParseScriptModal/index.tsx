bd4ca785bb52c5e0bf137c75ef3172a5c885fc91
ngoc.tran3 <ngoc.tran3@sotatek.com>
2023-08-09 15:12:22 +0700

    fix: transaction protocol update tooltip and popup issue

--- src/components/ParseScriptModal/index.tsx
+++ src/components/ParseScriptModal/index.tsx
@@ -1,9 +1,8 @@
-import { Box, useTheme, Modal } from "@mui/material";
+import { useTheme } from "@mui/material";
 import { JsonViewer } from "@textea/json-viewer";
 
-import { CloseIcon } from "src/commons/resources";
-
-import { ButtonClose, ModalContainer, ViewJson } from "./styles";
+import CustomModal from "../commons/CustomModal";
+import { SubTitle, ViewJson } from "./styles";
 
 interface ParseScriptModalProps {
   open: boolean;
@@ -12,43 +11,33 @@ interface ParseScriptModalProps {
   title?: string;
   subTitle?: string;
 }
-const ParseScriptModal: React.FC<ParseScriptModalProps> = ({ title, script, subTitle, ...props }) => {
+const ParseScriptModal: React.FC<ParseScriptModalProps> = ({ script, subTitle, ...props }) => {
   const theme = useTheme();
-  return (
-    <Modal {...props}>
-      <ModalContainer>
-        <ButtonClose onClick={props.onClose}>
-          <img src={CloseIcon} alt="icon close" />
-        </ButtonClose>
-        <Box
-          mb={3}
-          textAlign={"left"}
-          color={({ palette }) => palette.secondary.main}
-          fontSize="1.5rem"
-          fontWeight="bold"
-          fontFamily={'"Roboto", sans-serif '}
-        >
-          {title}
-        </Box>
 
-        {subTitle && (
-          <Box color={({ palette }) => palette.secondary.main} mb={1}>
-            {subTitle}
-          </Box>
-        )}
-        <ViewJson>
-          <JsonViewer
-            value={script ? script : ""}
-            displayObjectSize={false}
-            displayDataTypes={false}
-            enableClipboard={false}
-            collapseStringsAfterLength={false}
-            style={{ padding: 0, background: "none", color: theme.palette.text.secondary }}
-            rootName={false}
-          />
-        </ViewJson>
-      </ModalContainer>
-    </Modal>
+  const getObject = () => {
+    try {
+      const objectData = JSON.parse(script);
+      return objectData;
+    } catch (error) {
+      return script || "";
+    }
+  };
+
+  return (
+    <CustomModal {...props}>
+      {subTitle && <SubTitle>{subTitle}</SubTitle>}
+      <ViewJson maxHeight={subTitle ? "60vh" : "70vh"}>
+        <JsonViewer
+          value={getObject()}
+          displayObjectSize={false}
+          displayDataTypes={false}
+          enableClipboard={false}
+          collapseStringsAfterLength={false}
+          style={{ padding: 0, background: "none", color: theme.palette.text.secondary }}
+          rootName={false}
+        />
+      </ViewJson>
+    </CustomModal>
   );
 };
 
