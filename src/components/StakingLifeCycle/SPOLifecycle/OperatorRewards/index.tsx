c72bc4c2cc9308f138a2c2e5ada937ba2f878720
trung.hoang <trung.hoang@sotatek.com>
2023-08-30 10:48:10 +0700

    chore(style): MET-1352 change icon operator rewards

--- src/components/StakingLifeCycle/SPOLifecycle/OperatorRewards/index.tsx
+++ src/components/StakingLifeCycle/SPOLifecycle/OperatorRewards/index.tsx
@@ -3,7 +3,6 @@ import { Box, styled } from "@mui/material";
 import { useParams } from "react-router-dom";
 import moment from "moment";
 
-import { ADAOrangeBorderIcon } from "src/commons/resources";
 import CustomTooltip from "src/components/commons/CustomTooltip";
 import { details } from "src/commons/routers";
 import { formatADAFull, getShortWallet } from "src/commons/utils/helper";
@@ -17,6 +16,7 @@ import CardanoBlockchain from "src/components/commons/CardanoBlockchain";
 import SPOHolder from "src/components/commons/SPOHolder";
 import DrawPath from "src/components/commons/DrawPath";
 import { LineArrowItem } from "src/components/commons/LineArrow";
+import { ADAactiveFlip } from "src/components/commons/AdaActiveFlip";
 
 import { StyledLink, DrawContainer, ADAOperator, ADATitle, ADAAmount, StyledEpoch } from "./styles";
 
@@ -55,7 +55,7 @@ const OperatorReward = () => {
       <DrawContainer>
         <CardanoBlockchain ref={cardanoBlockchainRef} />
         <ADAOperator ref={operatorRef} onClick={() => setOpenModal(true)}>
-          <ADAOrangeBorderIcon />
+          <ADAactiveFlip />
           <ADATitle>Operator Rewards</ADATitle>
         </ADAOperator>
         <SPOHolder
