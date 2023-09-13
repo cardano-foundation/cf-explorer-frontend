af5fcd3098525819b493f5ccc01d37ef9e54467a
Truong Manh Nguyen (truong.nguyen4) <truong.nguyen4@sotatek.com>
2023-09-13 17:01:21 +0700

    fix: remove redudent language

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,15 +2,11 @@ import i18n, { use } from "i18next";
 import { initReactI18next } from "react-i18next";
 
 import en from "src/locales/en/translation.json";
-import fr from "src/locales/fr/translation.json";
 
 use(initReactI18next).init({
   resources: {
     en: {
       translation: en
-    },
-    fr: {
-      translation: fr
     }
   },
   lng: "en",
