e506103e2d0ae1ab60ee2cee0e4fdef0c6c414f6
Truong Manh Nguyen (truong.nguyen4) <truong.nguyen4@sotatek.com>
2023-09-18 11:32:41 +0700

    fix: MET-1485 fix route change language

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { I18nextProvider } from "react-i18next";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -9,13 +8,19 @@ import AppContainer from "./AppContainer";
 import Routers from "./Routers";
 import i18n from "./i18n";
 import store from "./stores";
+import { SUPPORTED_LANGUAGES } from "./commons/utils/constants";
 
 const App: React.FC = () => {
-  const lang = localStorage.getItem("lang") || "en";
+  let basename = "/";
+  const pattern = /^\/([a-z]{2})\//;
+  const currentLanguage = window.location.pathname.match(pattern)?.[1];
+  if (currentLanguage && SUPPORTED_LANGUAGES.includes(currentLanguage)) {
+    basename = `/${currentLanguage}`;
+  }
   return (
     <I18nextProvider i18n={i18n} defaultNS={"translation"}>
       <Provider store={store}>
-        <Router basename={lang}>
+        <Router basename={basename}>
           <AppContainer>
             <Routers />
           </AppContainer>
