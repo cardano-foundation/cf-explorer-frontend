4cac15b1ea1bd3b70917d1c70af5b9afb3e70dba
tai.truong <tai.truong@sotatek.com>
2023-09-25 15:35:42 +0700

    feat: MET-182 updatee color

--- src/components/commons/Layout/Sidebar/index.tsx
+++ src/components/commons/Layout/Sidebar/index.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useWindowSize } from "react-use";
+import { Box } from "@mui/material";
 
 import { RootState } from "src/stores/types";
 import { LogoFullIcon, LogoIcon, LogoDarkmodeFullIcon, LogoDarkmodeShortIcon } from "src/commons/resources";
+import { useScreen } from "src/commons/hooks/useScreen";
+import { setTheme } from "src/stores/user";
 
 import SidebarMenu from "./SidebarMenu";
 import { NavbarContainer, NavBarLogo, LogoLink, HeaderTop, NavbarMenuBottom } from "./styles";
 import SelectNetwork from "../Header/SelectNetwork";
+import { SwitchMode } from "../Header/styles";
 
 const Sidebar: React.FC = () => {
   const { sidebar, theme } = useSelector(({ user }: RootState) => user);
   const { height } = useWindowSize();
-
+  const { isGalaxyFoldSmall } = useScreen();
   const getLogo = () => {
     if (theme === "light") {
       if (sidebar) {
@@ -39,6 +43,16 @@ const Sidebar: React.FC = () => {
       <NavbarMenuBottom sidebar={+sidebar}>
         <SelectNetwork />
       </NavbarMenuBottom>
+      {isGalaxyFoldSmall && (
+        <Box p={"0 14px"}>
+          <SwitchMode
+            checked={theme === "dark"}
+            onChange={(e) => {
+              setTheme(e.target.checked ? "dark" : "light");
+            }}
+          />
+        </Box>
+      )}
     </NavbarContainer>
   );
 };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Button, Container, styled } from "@mui/material";
+import { Container, styled } from "@mui/material";
 import { useTranslation } from "react-i18next";
 
 import LatestStories from "src/components/Home/LatestStories";
@@ -7,7 +7,6 @@ import LatestTransactions from "src/components/Home/LatestTransactions";
 import HomeStatistic from "src/components/Home/Statistic";
 import TopDelegationPools from "src/components/Home/TopDelegationPools";
 import HomeTrending from "src/components/Home/Trending";
-import useToast from "src/commons/hooks/useToast";
 
 const HomeContainer = styled(Container)`
   padding-top: 30px;
@@ -16,20 +15,12 @@ const HomeContainer = styled(Container)`
 
 const Home = () => {
   const { t } = useTranslation();
-  const toast = useToast();
   useEffect(() => {
     document.title = t("head.page.dashboard");
   }, [t]);
 
   return (
     <HomeContainer data-testid="home-container">
-      <Button
-        onClick={() => {
-          toast.success("hi");
-        }}
-      >
-        click
-      </Button>
       <HomeStatistic />
       <HomeTrending />
       <LatestTransactions />
