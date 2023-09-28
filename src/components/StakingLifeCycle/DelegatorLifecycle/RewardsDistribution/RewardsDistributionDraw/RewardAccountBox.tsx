1d51a47b9dfc27a4ab362f30f072c5e6a64b83f2
Trung <trunghoang.dev@gmail.com>
2023-09-28 19:47:17 +0700

    fix: conflict reward account box

--- src/components/StakingLifeCycle/DelegatorLifecycle/RewardsDistribution/RewardsDistributionDraw/RewardAccountBox.tsx
+++ src/components/StakingLifeCycle/DelegatorLifecycle/RewardsDistribution/RewardsDistributionDraw/RewardAccountBox.tsx
@@ -1,4 +1,4 @@
-import { BoxProps, Typography } from "@mui/material";
+import { BoxProps, Typography, useTheme } from "@mui/material";
 import { forwardRef } from "react";
 import { useTranslation } from "react-i18next";
 
@@ -21,6 +21,7 @@ export interface IRewardAccountBoxProps extends BoxProps {
 
 const RewardAccountBox: React.FC<IRewardAccountBoxProps> = forwardRef(({ value, toggleRewardModal }, boxRef) => {
   const { t } = useTranslation();
+  const theme = useTheme();
   return (
     <RewardAccountCcontainer ref={boxRef} onClick={toggleRewardModal}>
       <RewardBoxImg src={GiftIcon} />
