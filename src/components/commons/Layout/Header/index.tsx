072ec93bde27c3a29449c14bc1f41af3e850d545
Truong Manh Nguyen (truong.nguyen4) <truong.nguyen4@sotatek.com>
2023-09-29 11:03:11 +0700

    feat: MET-182 darkmode details for Staking Lifecycle homepage

--- src/components/commons/Layout/Header/index.tsx
+++ src/components/commons/Layout/Header/index.tsx
@@ -14,6 +14,7 @@ import {
 import { lists, routers } from "src/commons/routers";
 import { setOnDetailView, setSidebar, setTheme } from "src/stores/user";
 
+import CustomIcon from "../../CustomIcon";
 import TopSearch from "../Sidebar/TopSearch";
 import HeaderSearch from "./HeaderSearch";
 import LoginButton from "./LoginButton";
@@ -33,7 +34,6 @@ import {
   SwitchMode,
   Title
 } from "./styles";
-import CustomIcon from "../../CustomIcon";
 
 const HIDDEN_HEADER_SEARCH_PATHS: string[] = [lists.dashboard()];
 
