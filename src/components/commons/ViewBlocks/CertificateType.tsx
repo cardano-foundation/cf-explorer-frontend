64613790c470f490f48e4359055d211d505bd223
Sotatek-TungPham <tung.pham@sotatek.com>
2023-10-05 10:19:45 +0700

    feat: update langs

--- src/components/commons/ViewBlocks/CertificateType.tsx
+++ src/components/commons/ViewBlocks/CertificateType.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Typography } from "@mui/material";
+import { useTranslation } from "react-i18next";
 
 import { CertValueBox, CertificateTypeBox } from "./styles";
 
 const CertificateType: React.FC<{ redeemerCertType?: IContractItemTx["redeemerCertType"] }> = ({
   redeemerCertType = "DELEGATION"
 }) => {
+  const { t } = useTranslation();
   const certType = {
     DELEGATION: "Stake Registration",
     STAKE_DEREGISTRATION: "Stake Address Deregistrations"
@@ -13,7 +15,7 @@ const CertificateType: React.FC<{ redeemerCertType?: IContractItemTx["redeemerCe
   return (
     <CertificateTypeBox>
       <Typography fontSize="1rem" fontWeight={600}>
-        Certificate Type:
+        {t("contract.certificateType")}:
       </Typography>
       <CertValueBox>{certType[redeemerCertType]}</CertValueBox>
     </CertificateTypeBox>
