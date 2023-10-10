7a220bb177c8247df1965ff25b295762b52fc20e
Sotatek-TungPham <tung.pham@sotatek.com>
2023-10-10 15:27:32 +0700

    feat: MET-1635 Pools page enhancements (support ticket CF-178)

--- src/components/DelegationPool/DelegationList/styles.ts
+++ src/components/DelegationPool/DelegationList/styles.ts
@@ -1,4 +1,4 @@
-import { styled, Button, LinearProgress } from "@mui/material";
+import { styled, Button, LinearProgress, Box, Switch } from "@mui/material";
 import { Link } from "react-router-dom";
 
 export const StyledLinearProgress = styled(LinearProgress)<{ saturation: number }>`
@@ -29,7 +29,6 @@ export const SearchContainer = styled("div")(({ theme }) => ({
   background: theme.palette.secondary[0],
   padding: "0 12px",
   borderRadius: 8,
-  marginBottom: 15,
   height: 35,
   border: `1.5px solid ${theme.mode === "light" ? theme.palette.primary[200] : theme.palette.secondary[700]}`,
   "&:focus-within": {
@@ -65,3 +64,74 @@ export const Image = styled("img")`
   width: 20px;
   height: 20px;
 `;
+
+export const TopSearchContainer = styled(Box)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 15px;
+  ${({ theme }) => theme.breakpoints.down("sm")} {
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 15px;
+  }
+`;
+
+export const ShowRetiredPools = styled(Box)`
+  font-size: 14px;
+  font-weight: 700;
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.palette.secondary.light};
+  gap: 12px;
+  ${({ theme }) => theme.breakpoints.down("sm")} {
+    width: 100%;
+    justify-content: flex-end;
+  }
+`;
+
+export const AntSwitch = styled(Switch)(({ theme }) => ({
+  width: 48,
+  height: 24,
+  padding: 0,
+  display: "flex",
+  borderRadius: 77,
+  "&:active": {
+    "& .MuiSwitch-thumb": {
+      width: 18
+    },
+    "& .MuiSwitch-switchBase.Mui-checked": {
+      transform: "translateX(23px)"
+    }
+  },
+  "& .MuiSwitch-switchBase": {
+    padding: 2,
+    transform: "translateX(2px)",
+    "&.Mui-checked": {
+      transform: "translateX(23px)",
+      color: "#fff",
+      "& + .MuiSwitch-track": {
+        opacity: 1,
+        backgroundColor: theme.palette.primary[200]
+      },
+      "& .MuiSwitch-thumb": {
+        backgroundColor: theme.palette.primary.main
+      }
+    }
+  },
+  "& .MuiSwitch-thumb": {
+    boxShadow: "0 2px 4px 0 rgb(0 35 11 / 20%)",
+    width: 19,
+    height: 19,
+    borderRadius: 9,
+    transition: theme.transitions.create(["width", "background"], {
+      duration: 200
+    })
+  },
+  "& .MuiSwitch-track": {
+    borderRadius: 19 / 2,
+    opacity: 1,
+    backgroundColor: theme.palette.secondary[600],
+    boxSizing: "border-box"
+  }
+}));
