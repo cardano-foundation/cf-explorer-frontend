6456c2aea2324229f4412ab53095b4f40389f019
tai.truong <tai.truong@sotatek.com>
2023-10-20 15:02:56 +0700

    fix: MET-182 fix scroll tab trx detail

--- src/components/TransactionDetail/TransactionMetadata/Delegations/index.tsx
+++ src/components/TransactionDetail/TransactionMetadata/Delegations/index.tsx
@@ -20,7 +20,7 @@ const Delegations: React.FC<DelegationProps> = ({ data }) => {
     <Wrapper>
       <Header>{t("common.stakeAddress")}</Header>
       {data?.map((item) => (
-        <StyledItem key={item.address} overflow={"scroll"}>
+        <StyledItem key={item.address} overflow={"auto"}>
           <ItemContainer>
             <Box display="flex" alignItems="center">
               <Box width={50}>
