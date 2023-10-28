02e839f5e6d6c783a0ba802f53f9a1bc547e6f54
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2023-10-28 10:55:29 +0200

    removing cypress

--- cypress/e2e/stakingLifecycle/StakingLifecycleTest.cy.ts
+++
@@ -1,8 +0,0 @@
-describe("Deregistration screen", () => {
-    it("User go to Dashboard and click Staking Lifecycle in left tab then verify the Staking Lifecycle Screen", () => {
-      cy.visit("/");
-      cy.get('[data-testid="menu-button-staking_lifecycle"]').click();
-      cy.get('input[placeholder]').should('be.visible');
-      cy.get('p[class]').contains('Search to explore the staking lifecycle events of a delegator or pool.').should('be.visible');
-    });
-  });
