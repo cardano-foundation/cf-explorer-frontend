4a62cf99086b4ef6a1e0f205ae44d7125fd17b7d
tai.truong <tai.truong@sotatek.com>
2023-11-01 17:05:26 +0700

    feat: MET-1733 update search

--- src/commons/utils/api.ts
+++ src/commons/utils/api.ts
@@ -26,7 +26,7 @@ export const API = {
     DETAIL: "epochs",
     LIST: "epochs"
   },
-  POLICY: "policies",
+  POLICY: "scripts/search",
   POOL: "pools",
 
   TOKEN: { LIST: "tokens", TOKEN_TRX: "tokens/:tokenId/txs", ANALYTICS: "tokens/analytics" },
