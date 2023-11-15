d47f1006915764ee64516b2484f910632e976109
tai.truong <tai.truong@sotatek.com>
2023-11-15 11:25:14 +0700

    fix: MET-1727 update color and icon pool cert history

--- src/components/DelegationDetail/DelegationDetailList/styles.ts
+++ src/components/DelegationDetail/DelegationDetailList/styles.ts
@@ -1,48 +1,7 @@
 import { styled } from "@mui/material";
 import { Link } from "react-router-dom";
 
-import { POOL_ACTION_TYPE } from "src/commons/utils/constants";
-
 export const StyledLink = styled(Link)`
   color: ${(props) => props.theme.palette.primary.main} !important;
   font-family: var(--font-family-text) !important;
-`;
-
-export const PoolActionMark = styled("small")<{ actionType?: PoolActionType }>`
-  color: ${({ actionType, theme }) => {
-    switch (actionType) {
-      case POOL_ACTION_TYPE.POOL_DE_REGISTRATION:
-        return theme.palette.error[700];
-      case POOL_ACTION_TYPE.POOL_UPDATE:
-        return theme.palette.warning[800];
-      case POOL_ACTION_TYPE.POOL_REGISTRATION:
-        return theme.palette.success[800];
-      default:
-        return theme.palette.success[800];
-    }
-  }};
-  background-color: ${({ actionType, theme }) => {
-    switch (actionType) {
-      case POOL_ACTION_TYPE.POOL_DE_REGISTRATION:
-        return theme.palette.error[100];
-      case POOL_ACTION_TYPE.POOL_UPDATE:
-        return theme.palette.warning[100];
-      case POOL_ACTION_TYPE.POOL_REGISTRATION:
-        return theme.palette.success[100];
-      default:
-        return theme.palette.success[100];
-    }
-  }};
-  font-weight: var(--font-weight-bold);
-  text-transform: uppercase;
-  padding: 5px 7px;
-  border-radius: 2px;
-  font-size: 0.8125rem;
-  line-height: 1;
-  width: max-content;
-  white-space: no-wrap;
-  ${({ theme }) => theme.breakpoints.down("md")} {
-    padding: 3px 3px;
-    font-size: 0.75rem;
-  }
 `;
