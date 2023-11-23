4ed9cf5625de5c6746af332ba7a790c037ba7973
Tien Dang <tien.dang@sotatek.com>
2023-11-23 09:42:43 +0700

    fix: MET-1535: fix UI latest story when loading

--- src/components/Home/LatestStories/index.tsx
+++ src/components/Home/LatestStories/index.tsx
@@ -55,7 +55,7 @@ const LatestStories = () => {
 
   const onAfterChange: Settings["afterChange"] = (currentSlide) => {
     drag.current = false;
-    if (currentSlide >= data.length - 4 && data.length < total && !loading) {
+    if (currentSlide >= data.length && !loading) {
       setOffset(data.length);
     }
   };
@@ -105,53 +105,54 @@ const LatestStories = () => {
           { breakpoint: breakpoints.values.sm, settings: { slidesToShow: 1.5, slidesToScroll: 1 } }
         ]}
       >
-        {loading || !data.length
-          ? new Array(4).fill(0).map((_, index) => (
-              <Item key={index}>
-                <Box component={CommonSkeleton} variant="rectangular" borderRadius={2} height={132} />
-                <Box component={CommonSkeleton} variant="rectangular" borderRadius={2} height={15} mt={1} width="70%" />
-                <Box component={CommonSkeleton} variant="rectangular" borderRadius={2} height={15} mt={1} width="80%" />
-                <Box component={CommonSkeleton} variant="rectangular" borderRadius={2} height={80} mt={1} />
-                <FooterCard>
-                  <Box component={CommonSkeleton} variant="rectangular" borderRadius={2} height={20} width="60%" />
-                </FooterCard>
+        {data.map(({ meta_image, banner_image, title, news_item_content, resource_href }) => {
+          const { date, default_content, author } = news_item_content;
+
+          const authorName = getAuthorName(author);
+
+          return (
+            <a
+              key={date}
+              href={resource_href}
+              target="_blank"
+              rel="noreferrer"
+              onClick={(e) => drag.current && e.preventDefault()}
+            >
+              <Item>
+                <Image src={meta_image || banner_image} alt={title} />
+                <Detail>
+                  <WrapHeader>
+                    <CustomTooltip title={authorName}>
+                      <Author>{authorName}</Author>
+                    </CustomTooltip>
+                    <ResourceHref>{getHostname(resource_href)}</ResourceHref>
+                  </WrapHeader>
+                  <ItemTitle>{title} </ItemTitle>
+                  <Description>{default_content}</Description>
+                  <FooterCard>
+                    <Time>
+                      <TimeIcon src={CalenderPaleIcon} alt="calender pale" />
+                      {formatDateTime(date)}
+                    </Time>
+                  </FooterCard>
+                </Detail>
               </Item>
-            ))
-          : data.map(({ meta_image, banner_image, title, news_item_content, resource_href }) => {
-              const { date, default_content, author } = news_item_content;
+            </a>
+          );
+        })}
 
-              const authorName = getAuthorName(author);
-
-              return (
-                <a
-                  key={date}
-                  href={resource_href}
-                  target="_blank"
-                  rel="noreferrer"
-                  onClick={(e) => drag.current && e.preventDefault()}
-                >
-                  <Item>
-                    <Image src={meta_image || banner_image} alt={title} />
-                    <Detail>
-                      <WrapHeader>
-                        <CustomTooltip title={authorName}>
-                          <Author>{authorName}</Author>
-                        </CustomTooltip>
-                        <ResourceHref>{getHostname(resource_href)}</ResourceHref>
-                      </WrapHeader>
-                      <ItemTitle>{title} </ItemTitle>
-                      <Description>{default_content}</Description>
-                      <FooterCard>
-                        <Time>
-                          <TimeIcon src={CalenderPaleIcon} alt="calender pale" />
-                          {formatDateTime(date)}
-                        </Time>
-                      </FooterCard>
-                    </Detail>
-                  </Item>
-                </a>
-              );
-            })}
+        {data.length < total &&
+          new Array(4).fill(0).map((_, index) => (
+            <Item key={index}>
+              <Box component={CommonSkeleton} variant="rectangular" borderRadius={2} height={132} />
+              <Box component={CommonSkeleton} variant="rectangular" borderRadius={2} height={15} mt={1} width="70%" />
+              <Box component={CommonSkeleton} variant="rectangular" borderRadius={2} height={15} mt={1} width="80%" />
+              <Box component={CommonSkeleton} variant="rectangular" borderRadius={2} height={80} mt={1} />
+              <FooterCard>
+                <Box component={CommonSkeleton} variant="rectangular" borderRadius={2} height={20} width="60%" />
+              </FooterCard>
+            </Item>
+          ))}
       </StyledSlider>
     </LatestStoriesContainer>
   );
