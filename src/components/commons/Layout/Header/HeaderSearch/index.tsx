80e5c904f51950e6a05d25cbf655718ee8b3c217
tai.truong <tai.truong@sotatek.com>
2023-12-07 16:05:16 +0700

    feat: MET-1644 hidden dropdown when search in mobile

--- src/components/commons/Layout/Header/HeaderSearch/index.tsx
+++ src/components/commons/Layout/Header/HeaderSearch/index.tsx
@@ -202,6 +202,7 @@ const HeaderSearch: React.FC<Props> = ({ home, callback, setShowErrorMobile, his
       ) {
         handleSetSearchValueDefault();
         setLoading(false);
+        callback?.();
         setShowOption(false);
         if (adaHanlde?.stakeAddress) {
           history.push(`${details.stake(search)}?isADAHanlde=true`);
@@ -214,6 +215,7 @@ const HeaderSearch: React.FC<Props> = ({ home, callback, setShowErrorMobile, his
       if (!adaHanlde.paymentAddress) {
         if (keyDetail) {
           handleSetSearchValueDefault();
+          callback?.();
           handleRedirectDetail(keyDetail, res?.data);
           setLoading(false);
           setShowOption(false);
@@ -402,6 +404,7 @@ const HeaderSearch: React.FC<Props> = ({ home, callback, setShowErrorMobile, his
       const dataHanlde = await adaHandleSearch(search);
       if (dataHanlde) {
         handleSetSearchValueDefault();
+        callback?.();
         if (dataHanlde.stakeAddress) {
           history.push(`${details.stake(search)}?isADAHanlde=true`);
         } else {
@@ -414,6 +417,7 @@ const HeaderSearch: React.FC<Props> = ({ home, callback, setShowErrorMobile, his
           } else {
             await defaultAxios.get(URL_FETCH_DETAIL["addresses"](search)).then((data) => data.data);
           }
+          callback?.();
           if (search.startsWith("stake")) {
             history.push(details.stake(search));
             handleSetSearchValueDefault();
@@ -580,6 +584,7 @@ const HeaderSearch: React.FC<Props> = ({ home, callback, setShowErrorMobile, his
           setShowOption={setShowOption}
           dataSearchTokensAndPools={dataSearchTokensAndPools}
           ADAHandleOption={ADAHandleOption}
+          callback={callback}
         />
       )}
       {loading && search ? (
@@ -607,6 +612,7 @@ interface OptionProps {
   show: boolean;
   home: boolean;
   value: string;
+  callback?: () => void;
   error: string;
   data?: IResponseSearchAll;
   dataSearchTokensAndPools?: TokensSearch[] | DelegationPool[];
@@ -635,7 +641,8 @@ export const OptionsSearch = ({
   dataSearchTokensAndPools,
   totalResult,
   ADAHandleOption,
-  handleSetSearchValueDefault
+  handleSetSearchValueDefault,
+  callback
 }: OptionProps) => {
   const history = useHistory();
   const listOptionsTokensAndPools = dataSearchTokensAndPools?.map((i) => {
@@ -879,6 +886,7 @@ export const OptionsSearch = ({
                 onClick={() => {
                   setShowOption(false);
                   handleSetSearchValueDefault();
+                  callback?.();
                   filter === "tokens"
                     ? history.push(`${routers.TOKEN_LIST}?tokenName=${(value || "").toLocaleLowerCase()}`)
                     : history.push(routers.DELEGATION_POOLS, {
@@ -921,6 +929,7 @@ export const OptionsSearch = ({
                   setShowOption(false);
                   handleSetSearchValueDefault();
                   item?.cb?.();
+                  callback?.();
                 }}
                 data-testid="option-search-epoch"
               >
@@ -941,6 +950,7 @@ export const OptionsSearch = ({
               onClick={() => {
                 setShowOption(false);
                 handleSetSearchValueDefault();
+                callback?.();
                 if (ADAHandleOption?.stakeAddress) {
                   history.push(`${details.stake(value)}?isADAHanlde=true`);
                 } else {
