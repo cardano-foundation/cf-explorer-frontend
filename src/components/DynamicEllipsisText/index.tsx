ac6e0f8db5d9994e335d53184fccaf4ff29d6893
tu.nguyen5 <tu.nguyen5@sotatek.com>
2023-12-11 10:49:18 +0700

    fix: fix tooltip in transaction detail on mobile

--- src/components/DynamicEllipsisText/index.tsx
+++ src/components/DynamicEllipsisText/index.tsx
@@ -59,7 +59,8 @@ const DynamicEllipsisText = ({
   sxLastPart,
   sx,
   customTruncateFold,
-  isNoLimitPixel
+  isNoLimitPixel,
+  isSeparateCopyIcon
 }: {
   value: string;
   postfix?: number;
@@ -71,6 +72,7 @@ const DynamicEllipsisText = ({
   sx?: SxProps<Theme>;
   customTruncateFold?: [number, number];
   isNoLimitPixel?: boolean;
+  isSeparateCopyIcon?: boolean;
 }) => {
   const randomIdRef = useRef(`ELIPSIS_${useId()}`);
 
@@ -103,6 +105,28 @@ const DynamicEllipsisText = ({
   const lastPart = value?.slice(-postfix);
 
   if (isMin && !isNoLimitPixel) {
+    if (isSeparateCopyIcon) {
+      return (
+        <Box>
+          <CustomTooltip title={isTooltip ? <ScrollTooltipContent>{value}</ScrollTooltipContent> : ""}>
+            <ContainerShortHand
+              id={randomIdRef.current}
+              data-testid="ellipsis-text"
+              sx={{
+                display: "inline",
+                ...sx
+              }}
+            >
+              {customTruncateFold?.length === 2 && isGalaxyFoldSmall
+                ? truncateCustom(value, customTruncateFold[0], customTruncateFold[1])
+                : getShortHash(value)}
+              {afterElm && <StyledAfterElm>{afterElm}</StyledAfterElm>}
+            </ContainerShortHand>
+          </CustomTooltip>
+          {isCopy && <CopyButton text={value} />}
+        </Box>
+      );
+    }
     return (
       <CustomTooltip title={isTooltip ? <ScrollTooltipContent>{value}</ScrollTooltipContent> : ""}>
         <ContainerShortHand id={randomIdRef.current} data-testid="ellipsis-text" sx={sx}>
