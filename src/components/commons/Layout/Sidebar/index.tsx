248a619acad6b80d0115c1bab48767fe4b49fa8a
tai.truong <tai.truong@sotatek.com>
2023-12-13 21:17:06 +0700

    fix: MET-1843 update switch mode button mobile

--- src/components/commons/Layout/Sidebar/index.tsx
+++ src/components/commons/Layout/Sidebar/index.tsx
@@ -1,22 +1,30 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useWindowSize } from "react-use";
+import { Box, Button, ButtonGroup, useTheme } from "@mui/material";
 
 import { useScreen } from "src/commons/hooks/useScreen";
-import { LogoDarkmodeFullIcon, LogoDarkmodeShortIcon, LogoFullIcon, LogoIcon } from "src/commons/resources";
+import {
+  DarkModeMobile,
+  LightModeMobile,
+  LogoDarkmodeFullIcon,
+  LogoDarkmodeShortIcon,
+  LogoFullIcon,
+  LogoIcon
+} from "src/commons/resources";
 import { setTheme } from "src/stores/theme";
 import { RootState } from "src/stores/types";
 
 import SelectNetwork from "../Header/SelectNetwork";
-import { SwitchMode } from "../Header/styles";
 import SidebarMenu from "./SidebarMenu";
 import { HeaderTop, LogoLink, NavBarLogo, NavbarContainer, NavbarMenuBottom, WrapButtonSelect } from "./styles";
 
 const Sidebar: React.FC = () => {
   const { sidebar } = useSelector(({ user }: RootState) => user);
   const { theme } = useSelector(({ theme }: RootState) => theme);
-  const { isMobile } = useScreen();
+  const { isTablet, isMobile } = useScreen();
   const { height } = useWindowSize();
+  const muiTheme = useTheme();
   const getLogo = () => {
     if (theme === "light") {
       if (sidebar) {
@@ -43,17 +51,46 @@ const Sidebar: React.FC = () => {
       <NavbarMenuBottom sidebar={+sidebar}>
         <SelectNetwork />
       </NavbarMenuBottom>
-      {isMobile && (
+      {isTablet && (
         <WrapButtonSelect>
-          <SwitchMode
-            sx={{
-              marginLeft: "-8px"
-            }}
-            checked={theme === "dark"}
-            onChange={(e) => {
-              setTheme(e.target.checked ? "dark" : "light");
-            }}
-          />
+          <ButtonGroup fullWidth={isMobile} variant="outlined" aria-label="outlined primary button group">
+            <Box
+              component={Button}
+              textTransform={"capitalize"}
+              onClick={() => {
+                setTheme("light");
+              }}
+              borderColor={muiTheme.isDark ? muiTheme.palette.primary[100] : muiTheme.palette.primary.main}
+              color={muiTheme.isDark ? muiTheme.palette.secondary.light : muiTheme.palette.primary.main}
+              bgcolor={muiTheme.isDark ? "transparent" : muiTheme.palette.primary[200]}
+              fontSize={16}
+            >
+              <Box
+                component={LightModeMobile}
+                mr={"4px"}
+                fill={muiTheme.isDark ? muiTheme.palette.secondary.light : muiTheme.palette.primary.main}
+              />
+              Light
+            </Box>
+            <Box
+              component={Button}
+              textTransform={"capitalize"}
+              onClick={() => {
+                setTheme("dark");
+              }}
+              fontSize={16}
+              borderColor={muiTheme.isDark ? muiTheme.palette.primary.main : muiTheme.palette.primary[200]}
+              color={muiTheme.isDark ? muiTheme.palette.primary.main : muiTheme.palette.secondary.light}
+              bgcolor={muiTheme.isDark ? muiTheme.palette.secondary[0] : "transparent"}
+            >
+              <Box
+                component={DarkModeMobile}
+                mr={"4px"}
+                fill={muiTheme.isDark ? muiTheme.palette.primary.main : muiTheme.palette.secondary.light}
+              />
+              Dark
+            </Box>
+          </ButtonGroup>
         </WrapButtonSelect>
       )}
     </NavbarContainer>
