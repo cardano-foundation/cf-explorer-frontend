a3a812ac280c4200587b092006418afa107e4d15
Tien Dang <tien.dang@sotatek.com>
2023-12-21 15:36:44 +0700

    fix: fix cypress test

--- cypress/e2e/contracts.cy.ts
+++ cypress/e2e/contracts.cy.ts
@@ -4,7 +4,8 @@ describe("contracts spec", () => {
   it("should show the contract details of transaction 4223b55d78e90917c5a6054c8a038124f7b1c0ecefc6751dd8bcd6a5016ae2d1", () => {
     const transactionHash = "4223b55d78e90917c5a6054c8a038124f7b1c0ecefc6751dd8bcd6a5016ae2d1";
     cy.visit(`/transaction/${transactionHash}/contracts`);
-    cy.get(".MuiAccordionSummary-content div").should("contain", `Contracts`);
+    cy.wait(1000);
+    cy.get(".MuiAccordionSummary-content div").contains("contracts", { matchCase: false });
   });
 
   it("redirect to correct address detail page", () => {
