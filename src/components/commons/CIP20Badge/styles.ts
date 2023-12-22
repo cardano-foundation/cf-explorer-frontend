f0a001a471e1c929a2a5bd975b81bdecd9be6c81
tai.truong <tai.truong@sotatek.com>
2023-12-22 11:28:03 +0700

    feat: MET-1498 update color badge

--- src/components/commons/CIP20Badge/styles.ts
+++ src/components/commons/CIP20Badge/styles.ts
@@ -2,17 +2,19 @@ import { Box, Typography, styled } from "@mui/material";
 
 export const BadgeContainer = styled(Box)<{ success: number }>`
   border-radius: 10px;
-  background-color: ${({ theme, success }) => (success ? theme.palette.success[100] : theme.palette.warning[100])};
+  background-color: ${({ theme, success }) => (success ? theme.palette.success[100] : theme.palette.primary[100])};
   display: flex;
   justify-content: space-between;
   align-items: center;
   height: 24px;
-  border: 1px solid ${({ theme, success }) => (success ? theme.palette.success[800] : theme.palette.warning[800])};
+  border: 1px solid ${({ theme, success }) => (success ? theme.palette.success[800] : theme.palette.secondary[600])};
   gap: 6px;
+  width: auto;
   padding-right: 8px;
+  padding-left: ${({ success }) => (success ? 0 : 2)}px;
   box-sizing: border-box;
-  min-width: 78px;
   cursor: pointer;
+  min-width: 78px;
 `;
 
 export const CIPLabel = styled(Typography)`
