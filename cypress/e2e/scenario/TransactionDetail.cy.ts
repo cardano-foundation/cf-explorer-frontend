dd04842e7b15b193bd8c185b46779d2afffab64c
Tien Dang <tien.dang@sotatek.com>
2023-12-25 14:58:56 +0700

    feat: MET-1849: fix e2e test

--- cypress/e2e/scenario/TransactionDetail.cy.ts
+++ cypress/e2e/scenario/TransactionDetail.cy.ts
@@ -22,12 +22,11 @@ describe("Pool Certificate", () => {
     cy.get('[data-test-id="CircularProgressbarWithChildren__children"]').should("be.visible");
   });
 
-  it.only("should display CIP-60 in metadata tab", () => {
+  it("should display CIP-60 in metadata tab", () => {
     const transaction = "a810ba9f89dc9e2aedbd722115c684dab646e3e9956b495b02ece5338cbc5c4a";
     cy.visit("/transactions");
     cy.get('[data-testid="search-bar"]').type(transaction).type("{enter}");
     cy.wait(1000);
-    cy.get("div").contains("transaction details", { matchCase: false }).should("be.visible");
     cy.get('[data-testid="metadata-tab"]').click();
     cy.wait(1000);
     cy.get(".MuiAccordion-region div").contains("Metadata Hash", { matchCase: false }).should("be.visible");
