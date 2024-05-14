f169cd2338bfdd112945c5f7dd99c66ba7f27abf
Sotatek-TungNguyen6 <tung.nguyen6@sotatek.com>
2024-05-14 11:00:26 +0700

    fix: update requirment

--- src/components/BlockDetail/BlockOverview/index.tsx
+++ src/components/BlockDetail/BlockOverview/index.tsx
@@ -135,7 +135,7 @@ const BlockOverview: React.FC<BlockOverviewProps> = ({ data, loading, lastUpdate
       value: (
         <Box
           component={data?.poolView ? Link : Box}
-          color={({ palette }) => `${palette.primary.main}!important`}
+          color={({ palette }) => `${data?.poolView ? palette.primary.main : palette.text}!important`}
           to={details.delegation(data?.poolView)}
         >
           {data?.poolName ? (
