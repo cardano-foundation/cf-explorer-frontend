3d8202cbdb058a7744b2e61d7163852ee3682636
huy.doan <huy.doan@sotatek.com>
2024-06-19 17:07:55 +0700

    feat: MET-2220 Transaction history from genesis

--- src/components/Home/Trending/TransactionChart/index.tsx
+++ src/components/Home/Trending/TransactionChart/index.tsx
@@ -294,6 +294,7 @@ const Chart = ({ data, range }: { data: TransactionChartIF[] | null; range: Time
             tickLine={{ stroke: themeMode === "light" ? theme.palette.secondary.light : theme.palette.secondary[800] }}
             dataKey="date"
             tickFormatter={(date: string) => formatX(date, range)}
+            minTickGap={20}
           />
           <YAxis
             tick={{ fill: themeMode === "light" ? theme.palette.secondary.light : theme.palette.secondary[800] }}
