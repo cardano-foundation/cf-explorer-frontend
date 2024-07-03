8ccc78a6e008237ad93f82b985b6a0480619d4f7
tung.nguyen6 <tung.nguyen6@sotatek.com>
2024-07-03 18:19:41 +0700

    fix: bug ui dreps and pools

--- src/components/Dreps/DrepsOverview/styles.ts
+++ src/components/Dreps/DrepsOverview/styles.ts
@@ -46,6 +46,9 @@ export const StyledCard = {
     box-shadow: ${(props) => props.theme.shadow.card};
     position: relative;
     display: flex;
+    ${({ theme }) => theme.breakpoints.down(350)} {
+      display: block;
+    }
   `,
   ClickAble: styled(Link)`
     height: 100%;
