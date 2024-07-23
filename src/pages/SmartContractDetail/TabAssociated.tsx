164cff932a6ad80364e1495ce69477ef425c6df8
tung.nguyen6 <tung.nguyen6@sotatek.com>
2024-07-23 14:16:41 +0700

    fix: sc detail scroll associated

--- src/pages/SmartContractDetail/TabAssociated.tsx
+++ src/pages/SmartContractDetail/TabAssociated.tsx
@@ -34,10 +34,7 @@ const TabAssociated: React.FC<TTabAssociatedProps> = ({ data, loading }) => {
   return (
     <Box>
       <StyledSubNameTab data-testid="sc.subNameTab">{t("AssociatedAddresses")}:</StyledSubNameTab>
-      <Box
-        maxHeight={380}
-        overflow={data?.associatedAddresses && data?.associatedAddresses.length > 1 ? "auto" : "initial"}
-      >
+      <Box maxHeight={380} overflow={"auto"} minHeight={24}>
         {renderData()}
       </Box>
     </Box>
