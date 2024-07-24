e3b9b9139d13af0a1b6411bf9fc0ddc45aeb867b
Sotatek-TungNguyen6 <tung.nguyen6@sotatek.com>
2024-07-24 17:52:44 +0700

    feat: MET-2247 block distribution per epoch chart

--- src/components/NetworkMonitoring/index.tsx
+++ src/components/NetworkMonitoring/index.tsx
@@ -2,14 +2,18 @@ import { Box } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import { styled } from "@mui/system";
 
+import { NETWORK, NETWORKS } from "src/commons/utils/constants";
+
 import Card from "../commons/Card";
 import RegisteredStakepoolsChart from "./RegisteredStakepools";
+import BlockPropagationChart from "./BlockPropagation";
 
 export default function NetworkMonitoringComponent() {
   const { t } = useTranslation();
   return (
     <WrapPage>
       <Card title={t("glossary.networkMonitoring")} />
+      {NETWORK === NETWORKS.mainnet && <BlockPropagationChart />}
       <RegisteredStakepoolsChart />
     </WrapPage>
   );
