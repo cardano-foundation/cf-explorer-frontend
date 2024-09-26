87161be95ea7e734b3935ab682e9dc2b087db7cf
huy.doan <huy.doan@sotatek.com>
2024-09-26 11:35:20 +0700

    fix: MET-2364 Get bolnisi numbers from API backend

--- src/commons/utils/api.ts
+++ src/commons/utils/api.ts
@@ -187,6 +187,9 @@ export const API = {
     CARBON_EMISSION: "MiCAR/carbon-emission",
     INDICATOR: "/MiCAR/carbon-emission/overview",
     HISTORYCAL: "/MiCAR/carbon-emission/historical"
+  },
+  BOLNISI: {
+    OVERVIEW: "bolnisi/overview"
   }
 };
 
