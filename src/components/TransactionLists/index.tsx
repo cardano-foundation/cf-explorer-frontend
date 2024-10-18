c45d067c1035ca1cec0c353e68e0eb1662061423
tai.truong <tai.truong@sotatek.com>
2024-10-18 14:10:43 +0700

    fix: MET-2352 fix test fail

--- src/components/TransactionLists/index.tsx
+++ src/components/TransactionLists/index.tsx
@@ -34,9 +34,16 @@ const TransactionList: React.FC<TransactionListProps> = ({ underline = false, ur
 
   const fetchData = useFetchList<Transactions>(url, { ...pageInfo }, false, blockKey);
   const mainRef = useRef(document.querySelector("#main"));
-  const onClickRow = (_: MouseEvent<Element, globalThis.MouseEvent>, r: Transactions) => {
+
+  const onClickRow = (e: MouseEvent<Element, globalThis.MouseEvent>, r: Transactions) => {
+    if (e.target instanceof HTMLSpanElement) {
+      e.preventDefault();
+      e.stopPropagation();
+      return;
+    }
     history.push(details.transaction(r.hash));
   };
+
   const { error } = fetchData;
   const columns: Column<Transactions>[] = [
     {
@@ -64,7 +71,10 @@ const TransactionList: React.FC<TransactionListProps> = ({ underline = false, ur
       render: (r, index) => {
         const { blockName, tooltip } = formatNameBlockNo(r.blockNo, r.epochNo) || getShortHash(r.blockHash);
         return (
-          <StyledLink to={details.block(r.blockNo || r.blockHash)}>
+          <StyledLink
+            to={details.block(r.blockNo || r.blockHash)}
+            data-testid={`transactions.table.block#${r.blockNo}`}
+          >
             <CustomTooltip title={tooltip}>
               <span data-testid={`transaction.table.value.block#${index}`}>{blockName}</span>
             </CustomTooltip>
