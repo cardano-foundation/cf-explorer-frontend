a0fba349e736e4a0afaebb4fa20227628c18c125
Sotatek-TungNguyen6 <tung.nguyen6@sotatek.com>
2024-11-15 11:30:17 +0700

    feat: MET-2390 Remove token icons and show them with nsfw button

--- src/components/TokenDetail/TokenOverview/index.tsx
+++ src/components/TokenDetail/TokenOverview/index.tsx
@@ -1,7 +1,10 @@
-import { Box } from "@mui/material";
+import { Box, Button, Modal, useTheme } from "@mui/material";
 import BigNumber from "bignumber.js";
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
+import ImageSearchIcon from "@mui/icons-material/ImageSearch";
+import HideImageIcon from "@mui/icons-material/HideImage";
+import { CgClose } from "react-icons/cg";
 
 import { FileGuard, SlotIcon, TimeIconComponent, USDIconComponent } from "src/commons/resources";
 import { formatDateTimeLocal, getShortHash, formatNumberTotalSupply, tokenRegistry } from "src/commons/utils/helper";
@@ -11,7 +14,16 @@ import CustomTooltip from "src/components/commons/CustomTooltip";
 import DatetimeTypeTooltip from "src/components/commons/DatetimeTypeTooltip";
 
 import ScriptModal from "../../ScriptModal";
-import { ButtonLink, PolicyId, PolicyScriptBtn, TokenDescription, TokenHeader, TokenUrl, WrapTitle } from "./styles";
+import {
+  ButtonLink,
+  CloseButton,
+  PolicyId,
+  PolicyScriptBtn,
+  TokenDescription,
+  TokenHeader,
+  TokenUrl,
+  WrapTitle
+} from "./styles";
 
 BigNumber.config({ DECIMAL_PLACES: 40 });
 
@@ -23,9 +35,13 @@ interface ITokenOverview {
 
 const TokenOverview: React.FC<ITokenOverview> = ({ data, loading, lastUpdated }) => {
   const { t } = useTranslation();
+  const [openLogoModal, setOpenLogoModal] = useState(false);
   const [openModal, setOpenModal] = useState(false);
   const [policyId, setPolicyId] = useState("");
+  const [showLogo, setShowLogo] = useState(false);
   const decimalToken = data?.decimals || data?.metadata?.decimals || 0;
+
+  const theme = useTheme();
 
   const listItem = [
     {
@@ -34,11 +50,119 @@ const TokenOverview: React.FC<ITokenOverview> = ({ data, loading, lastUpdated })
           <CustomTooltip title={data?.displayName || data?.fingerprint || ""}>
             <span>{data?.displayName || getShortHash(data?.fingerprint) || ""}</span>
           </CustomTooltip>
-          {data?.metadata && data?.metadata?.logo ? (
-            <Box component={"img"} width={"auto"} height={36} src={`${data.metadata.logo}`} alt="logo icon" ml={1} />
-          ) : (
-            ""
+          {!showLogo && (
+            <ImageSearchIcon
+              sx={{ marginLeft: "8px", width: "auto", height: "36px", cursor: "pointer" }}
+              onClick={() => setOpenLogoModal(true)}
+              color="primary"
+            />
           )}
+          {showLogo && data?.metadata?.logo ? (
+            <Box
+              component="img"
+              width="auto"
+              height={36}
+              src={data.metadata.logo}
+              alt="logo icon"
+              ml={1}
+              onClick={(e) => e.stopPropagation()}
+            />
+          ) : showLogo ? (
+            <HideImageIcon sx={{ marginLeft: "8px", width: "auto", height: "36px" }} />
+          ) : null}
+
+          <Modal
+            open={openLogoModal}
+            onClose={(e: React.MouseEvent) => {
+              e.stopPropagation();
+              setOpenLogoModal(false);
+            }}
+            onClick={(e: React.MouseEvent) => e.stopPropagation()}
+            sx={{ zIndex: 9999 }}
+          >
+            <Box
+              onClick={(e) => e.stopPropagation()}
+              sx={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                bgcolor: theme.palette.primary[100],
+                boxShadow: 24,
+                p: 4,
+                borderRadius: 2,
+                minWidth: {
+                  xs: "150px",
+                  sm: "350px"
+                },
+                textAlign: "center",
+                zIndex: 9999,
+                padding: "30px",
+                border: "none",
+                outline: "none"
+              }}
+            >
+              <Box
+                sx={{
+                  marginBottom: "20px",
+                  color: theme.palette.secondary.main,
+                  textAlign: "left",
+                  fontSize: "24px",
+                  fontWeight: "700"
+                }}
+              >
+                NSFW Content
+              </Box>
+              <CloseButton onClick={() => setOpenLogoModal(false)}>
+                <CgClose />
+              </CloseButton>
+              <Box sx={{ backgroundColor: theme.isDark ? "#000" : "#fff", borderRadius: "8px", padding: "16px" }}>
+                <Box
+                  sx={{
+                    maxWidth: "270px",
+                    textAlign: "left",
+                    marginBottom: "60px",
+                    fontSize: "16px",
+                    fontWeight: "600",
+                    color: theme.palette.secondary.light
+                  }}
+                >
+                  {t("token.confirmView")}
+                </Box>
+                <Box sx={{ display: "flex", justifyContent: "center", gap: "92px" }}>
+                  <Button
+                    variant="outlined"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      setShowLogo(false);
+                      setOpenLogoModal(false);
+                    }}
+                    sx={{
+                      padding: "12px 16px ",
+                      textTransform: "none",
+                      borderRadius: "8px",
+                      fontWeight: "700",
+                      backgroundColor: theme.palette.secondary.light,
+                      color: !theme.isDark ? "#fff" : "#000"
+                    }}
+                  >
+                    {t("common.cancel")}
+                  </Button>
+                  <Button
+                    variant="contained"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      setShowLogo(true);
+                      setOpenLogoModal(false);
+                    }}
+                    sx={{ padding: "12px 16px", textTransform: "none", borderRadius: "8px", fontWeight: "700" }}
+                  >
+                    {t("common.continue")}
+                  </Button>
+                </Box>
+              </Box>
+            </Box>
+          </Modal>
         </TokenHeader>
       ),
       value: (
@@ -75,7 +199,7 @@ const TokenOverview: React.FC<ITokenOverview> = ({ data, loading, lastUpdated })
           <PolicyScriptBtn
             data-testid="token.policyScript"
             onClick={() => {
-              setOpenModal(true);
+              setOpenLogoModal(true);
               setPolicyId(data?.policy || "");
             }}
           >
